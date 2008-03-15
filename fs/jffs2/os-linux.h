6f4e3ece42a3212d8639800ebbacac635c9d161f
Jason Liu <r64343@freescale.com>
2008-03-14 21:53:04 -0500

    UBUNTU: Do not use OOB with MLC NAND
    Required to enable mx51 flavour. Should be pushed upstream.
    
    This patch is coming from the MTD community.
    
    Original Freescale patch ID: ENGR00068619 JFFS2 community fix with not use OOB
    
    Signed-off-by: Jason Liu <r64343@freescale.com>
    Signed-off-by: Amit Kucheria <amit.kucheria@canonical.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- fs/jffs2/os-linux.h
+++ fs/jffs2/os-linux.h
@@ -110,7 +110,11 @@ static inline void jffs2_init_inode_info(struct jffs2_inode_info *f)
 #define jffs2_can_mark_obsolete(c) (c->mtd->flags & (MTD_BIT_WRITEABLE))
 #endif
 
+#ifdef CONFIG_ARCH_MXC_CANONICAL
+#define jffs2_cleanmarker_oob(c) (c->mtd->type == MTD_NANDFLASH && (c->mtd->flags & MTD_OOB_WRITEABLE))
+#else
 #define jffs2_cleanmarker_oob(c) (c->mtd->type == MTD_NANDFLASH)
+#endif
 
 #define jffs2_flash_write_oob(c, ofs, len, retlen, buf) ((c)->mtd->write_oob((c)->mtd, ofs, len, retlen, buf))
 #define jffs2_flash_read_oob(c, ofs, len, retlen, buf) ((c)->mtd->read_oob((c)->mtd, ofs, len, retlen, buf))
