36d53bb542341879df6216e1190ab0fcf49a6465
Ben Collins <ben.collins@canonical.com>
2008-08-13 15:39:06 -0400

    UBUNTU: ubuntu: Add GFS driver
    Description: GFS Cluster File System
    
    Url: http://git.fedorahosted.org/git/cluster.git
    
    Version: 2.03.06
    
    Updates for asm/semaphore.h to linux/semaphore.h
    Updates for permission() API changes.
    
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>
    Signed-off-by: Ben Collins <ben.collins@canonical.com>

---
+++ ubuntu/gfs/ops_vm.h
@@ -0,0 +1,7 @@
+#ifndef __OPS_VM_DOT_H__
+#define __OPS_VM_DOT_H__
+
+extern struct vm_operations_struct gfs_vm_ops_private;
+extern struct vm_operations_struct gfs_vm_ops_sharewrite;
+
+#endif /* __OPS_VM_DOT_H__ */
