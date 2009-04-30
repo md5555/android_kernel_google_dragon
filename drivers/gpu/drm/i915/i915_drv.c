1b81042fbb36ddd55d93a3fb8b3f51c31958820e
Tim Gardner <tim.gardner@canonical.com>
2009-04-30 12:20:31 -0600

    UBUNTU: SAUCE: Default to i915.modeset=0 if CONFIG_DRM_I915_KMS=y
    This will help with the transition to using a KMS enabled X driver.
    Load the i915 modules with i915.modeset=1 to enable KMS.
    
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- drivers/gpu/drm/i915/i915_drv.c
+++ drivers/gpu/drm/i915/i915_drv.c
@@ -36,7 +36,12 @@
 #include "drm_pciids.h"
 #include <linux/console.h>
 
+#if defined(CONFIG_DRM_I915_KMS)
+static unsigned int i915_modeset = 0;
+#else
 static unsigned int i915_modeset = -1;
+#endif
+
 module_param_named(modeset, i915_modeset, int, 0400);
 
 unsigned int i915_fbpercrtc = 0;
