9b3b50b413874e003c1d384563d6fb0d85e90edf
Luke Yelavich <themuso@ubuntu.com>
2009-06-17 21:33:33 +1000

    UBUNTU: [Config] ports - Import of ports architectures into kernel packaging infrastructure
    As per the request of the archive admins, this is the first commit to
    re-introduce the ports architectures back into the main linux kernel package.
    This commit integrates the extra build dependencies, debian-installer kernel
    information, ports kernel abi fetching, and rules files for the ports
    architectures. Further commits will add kernel configuration files, and
    config system changes for ports architectures.
    
    Signed-off-by: Luke Yelavich <themuso@ubuntu.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

---
+++ debian/rules.d/powerpc.mk
@@ -0,0 +1,15 @@
+build_arch	= powerpc
+header_arch	= $(build_arch)
+asm_link	= $(build_arch)
+defconfig	= pmac32_defconfig
+flavours	= powerpc powerpc-smp powerpc64-smp
+build_image	= vmlinux
+kernel_file	= $(build_image)
+install_file	= $(build_image)
+
+loader		= yaboot
+
+custom_flavours	=
+
+no_dumpfile = true
+skipdbg		= true
