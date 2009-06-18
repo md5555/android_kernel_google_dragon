2ec75b4e52aa5244e6dc2ba10affd1831bcd64be
Tim Gardner <tim.gardner@canonical.com>
2009-06-17 19:18:58 -0600

    UBUNTU: [Config] Implement i386 generic and generic-pae flavours
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- debian/rules.d/i386.mk
+++ debian/rules.d/i386.mk
@@ -2,11 +2,11 @@ build_arch	= i386
 header_arch	= x86_64
 asm_link	= x86
 defconfig	= defconfig
-flavours        = generic server
+flavours        = generic generic-pae
 build_image	= bzImage
 kernel_file	= arch/$(build_arch)/boot/bzImage
 install_file	= vmlinuz
 
-server_sub	= virtual
+generic-pae_sub	= virtual
 
 loader		= grub
