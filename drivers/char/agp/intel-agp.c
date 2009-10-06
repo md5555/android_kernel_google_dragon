378b8e9b866452112b5da30e9c64a048dc4c44dd
Tim Gardner <tim.gardner@canonical.com>
2009-10-06 17:29:11 -0600

    UBUNTU: SAUCE: Use MODULE_IMPORT macro to tie intel_agp to i915
    BugLink: http://bugs.launchpad.net/bugs/430694
    
    Symbolicly link i915 and intel_agp, forcing intel_agp to
    get loaded first.
    
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- drivers/char/agp/intel-agp.c
+++ drivers/char/agp/intel-agp.c
@@ -2448,5 +2448,6 @@ static void __exit agp_intel_cleanup(void)
 module_init(agp_intel_init);
 module_exit(agp_intel_cleanup);
 
+MODULE_EXPORT(intel_agp);
 MODULE_AUTHOR("Dave Jones <davej@redhat.com>");
 MODULE_LICENSE("GPL and additional rights");
