a0325e6fee6cfee49405e9c38e4bba637781c39b
Leann Ogasawara <leann.ogasawara@canonical.com>
2009-11-10 13:38:34 -0800

    Revert "drm/i915: add B43 chipset support"
    BugLink: http://bugs.launchpad.net/bugs/480144
    
    This reverts commit 44e4c15b9024f34fe5fa7326768d7c71516b7c7a.
    
    Revert in favor of upstream commit from stable.
    
    Signed-off-by: Leann Ogasawara <leann.ogasawara@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Acked-by: Andy Whitcroft <andy.whitcroft@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -854,7 +854,6 @@ extern int i915_wait_ring(struct drm_device * dev, int n, const char *caller);
 		       (dev)->pci_device == 0x2E12 || \
 		       (dev)->pci_device == 0x2E22 || \
 		       (dev)->pci_device == 0x2E32 || \
-		       (dev)->pci_device == 0x2E42 || \
 		       (dev)->pci_device == 0x0042 || \
 		       (dev)->pci_device == 0x0046)
 
@@ -867,7 +866,6 @@ extern int i915_wait_ring(struct drm_device * dev, int n, const char *caller);
 		     (dev)->pci_device == 0x2E12 || \
 		     (dev)->pci_device == 0x2E22 || \
 		     (dev)->pci_device == 0x2E32 || \
-		     (dev)->pci_device == 0x2E42 || \
 		     IS_GM45(dev))
 
 #define IS_IGDG(dev) ((dev)->pci_device == 0xa001)
