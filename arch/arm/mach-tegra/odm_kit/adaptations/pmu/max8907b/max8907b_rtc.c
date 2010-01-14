7b29cc4a7bcee5f0ca927baaa45a90931b45d88c
Bitan Biswas <bbiswas@nvidia.com>
2010-01-14 13:56:12 +0530

    tegra2 rtc: date/time non-persistent on boards
    Changes are as follows:
    1) TPS6586x PMU RTC(harmony) cannot store more than 34 years duration. Hence,
     RTC driver has been modified to use year 2009 as reference instead of
     the year 1970(used in linux by default). Change in tps6586x rtc
     implementation.
     Issue was Fixed in Bug 621031 earlier and checked into p4 by modifying
     tegra rtc driver. We are using a different approach now by only
     modifying tps6586x rtc driver. This way rtc drivers for other PMU
     like max8907b which can store as much 9999 years in RTC do not need
     to change.
    2) MAX8907B PMU RTC(whistler) driver was not storing day/month/year data
     earlier. Further, on every boot the RTC was being cleared. Fixed this
     in file max8907b_rtc.c
     Implementation specific to linux as uses library functions: mktime and
     rtc_time_to_tm
    3) MAX8907B PMU I2C read API implementation was not reading 4th byte. Changed
     this as DD, MM, YY1, YY2 information is being read now. Changed max8907b_i2c.c
    
    Bug 625990 : [Firefly2/Harmony] Date & Time is not persisting after reboot
    
    Tested on : Harmony + whistler. Date/time persists.
    	Note: Whistler rechargable battery needs to be charged to 2.4V for
    	RTC data to persist. Harmony has non-rechargable battery.
    
    Change-Id: I07377e14c03dc7a9240190253885541be7f9a7d1

--- arch/arm/mach-tegra/odm_kit/adaptations/pmu/max8907b/max8907b_rtc.c
+++ arch/arm/mach-tegra/odm_kit/adaptations/pmu/max8907b/max8907b_rtc.c
@@ -30,149 +30,213 @@
  *
  */
 
+#include <linux/time.h>
+#include <linux/rtc.h>
 #include "max8907b.h"
 #include "max8907b_rtc.h"
 #include "max8907b_i2c.h"
 #include "max8907b_reg.h"
 
-/** 
+/**
 * The Maxim 8907B does not have an RTC that simply counts
 * seconds from some time t0 (as defined by the OS API).
 * Instead, this RTC contains several BCD (Binary Coded Decimal)
 * registers, including: seconds, minutes, hours, days, day of
 * week, date, etc...  These registers account for leap year and
 * the various days of the month as well.
-* 
+*
 * Since the OS interpretation of seconds to a particular
 * date/time from some OS-defined t0 is unknown at this level of
 * the implementation, it is not possible to translate the given
 * seconds into these registers (at least, not without a
 * dependency on some OS-specific information).
-* 
-* Therefore, this implementation contains a static variable
-* (RtcDays) which is derived from the number of seconds given
-* when Max8907bRtcCountWrite() is called.  The seconds, minutes
-* and hours are then programmed to the RTC and used to keep
-* track of the current time within the day.
-* 
-* TO DO: Increment the day whenever it rolls over (requires
-* handling an interrupt at midnight each day).
+*
 */
 
 #define MAX8907B_SECONDS_PER_DAY    (60*60*24)
 #define MAX8907B_SECONDS_PER_HOUR   (60*60)
 #define MAX8907B_SECONDS_PER_MINUTE (60)
 
-static NvBool bRtcNotInitialized = NV_TRUE;
-static NvU32 RtcDays = 0;
+#define LINUX_RTC_BASE_YEAR 1900
 
-NvBool 
+static NvBool bRtcNotInitialized = NV_TRUE;
+
+NvBool
 Max8907bRtcCountRead(
-    NvOdmPmuDeviceHandle hDevice, 
+    NvOdmPmuDeviceHandle hDevice,
     NvU32* Count)
 {
     NvU32 data = 0;
     NvU32 BcdHours, BcdMinutes, BcdSeconds;
     NvU32 Hours, Minutes, Seconds;
+    NvU32 BcdDD, BcdMM, BcdYY1, BcdYY2;
+    NvU32 DD, MM, YY1, YY2, YYYY;
+    // struct rtc_time tm;
 
-    if (Max8907bRtcWasStartUpFromNoPower(hDevice) && bRtcNotInitialized)
+    *Count = 0;
+    // Read seconds, minute, hour and weekday data from RTC registers
+    if (Max8907bRtcI2cReadTime(hDevice, MAX8907B_RTC_SEC, &data))
     {
-        Max8907bRtcCountWrite(hDevice, 0);
-        *Count = 0;
-    }
-    else
-    {
-        if (Max8907bRtcI2cReadTime(hDevice, MAX8907B_RTC_SEC, &data))
+        NVODMPMU_PRINTF(("\n Read time data-sec=0x%x ", data));
+        // Extract seconds, minute and hour data from RTC registers read
+        BcdHours   = (data >>  8) & 0xFF;
+        BcdMinutes = (data >> 16) & 0xFF;
+        BcdSeconds = (data >> 24) & 0xFF;
+
+        // Convert BCT time into decimal values
+        Hours   = ((BcdHours   & 0xF0)>>4)*10 + (BcdHours   & 0xF);
+        Minutes = ((BcdMinutes & 0xF0)>>4)*10 + (BcdMinutes & 0xF);
+        Seconds = ((BcdSeconds & 0xF0)>>4)*10 + (BcdSeconds & 0xF);
+
+        // Read day, month, yy1 and yy2 data from RTC registers
+        if (Max8907bRtcI2cReadTime(hDevice, MAX8907B_RTC_DATE, &data))
         {
-
-            BcdHours   = (data >>  8) & 0xFF;
-            BcdMinutes = (data >> 16) & 0xFF;
-            BcdSeconds = (data >> 24) & 0xFF;
-
-            Hours   = ((BcdHours   & 0xF0)>>4)*10 + (BcdHours   & 0xF);
-            Minutes = ((BcdMinutes & 0xF0)>>4)*10 + (BcdMinutes & 0xF);
-            Seconds = ((BcdSeconds & 0xF0)>>4)*10 + (BcdSeconds & 0xF);
-
-            *Count = (Hours * MAX8907B_SECONDS_PER_HOUR) +
-                     (Minutes * MAX8907B_SECONDS_PER_MINUTE) + Seconds;
+            NVODMPMU_PRINTF(("\n Read time data-year=0x%x ", data));
+            // Extract day, month, yy1 and yy2 data from RTC registers read
+            BcdYY2   = (data & 0xFF);
+            BcdYY1   = (data >>  8) & 0xFF;
+            BcdMM = (data >> 16) & 0xFF;
+            BcdDD = (data >> 24) & 0xFF;
+            // convert bct day/month/year data to decimal values
+            YY2 = ((BcdYY2   & 0xF0)>>4)*10 + (BcdYY2   & 0xF);
+            YY1 = ((BcdYY1   & 0xF0)>>4)*10 + (BcdYY1   & 0xF);
+            YYYY = (YY2 * 100 + YY1) & 0xFFFF;
+            MM = ((BcdMM & 0xF0)>>4)*10 + (BcdMM & 0xF);
+            DD = ((BcdDD & 0xF0)>>4)*10 + (BcdDD & 0xF);
+            // get seconds since reference time value given
+            // year, month, day, hour, minutes and seconds
+            // NOTE: Using linux specific API mktime for conversion
+            *Count = mktime(YYYY, (MM + 1), DD, Hours, Minutes, Seconds);
+            NVODMPMU_PRINTF(("\n Rtc read count=0x%x ", *Count));
+            NVODMPMU_PRINTF(("\n mktime: YYYY=%d MM=%d DD=%d Hr=%d Min=%d "
+                "Sec=%d, *Count=0x%x ", YYYY, (MM + 1), DD, Hours, Minutes,
+                Seconds, *Count));
+#if NV_DEBUG
+            // Call to verify that reverse conversion of seconds matches date
+            rtc_time_to_tm(*Count, &tm);
+            // Check if Local_rtc_time_to_tm can return values sent to mktime
+            NVODMPMU_PRINTF(("\n rtc_time_to_tm: YYYY=%d MM=%d DD=%d Hr=%d "
+                "Min=%d Sec=%d, *Count=0x%x ", (tm.tm_year +
+                LINUX_RTC_BASE_YEAR), tm.tm_mon, tm.tm_mday, tm.tm_hour,
+                tm.tm_min, tm.tm_sec, *Count));
+#endif
         }
         else
         {
-            NvOdmOsDebugPrintf("Max8907bRtcCountRead() error. ");
+            NVODMPMU_PRINTF(("\n Max8907bRtcCountRead() error. "));
             return NV_FALSE;
         }
     }
+    else
+    {
+        NVODMPMU_PRINTF(("\n Max8907bRtcCountRead() error. "));
+        return NV_FALSE;
+    }
+    NVODMPMU_PRINTF(("\n *Count=0x%x ", *Count));
     return NV_TRUE;
 }
 
-NvBool 
+NvBool
 Max8907bRtcAlarmCountRead(
-    NvOdmPmuDeviceHandle hDevice, 
+    NvOdmPmuDeviceHandle hDevice,
     NvU32* Count)
 {
     return NV_FALSE;
 }
 
-NvBool 
+NvBool
 Max8907bRtcCountWrite(
-    NvOdmPmuDeviceHandle hDevice, 
+    NvOdmPmuDeviceHandle hDevice,
     NvU32 Count)
 {
-    NvU32 Hours, Minutes, Seconds;
     NvU32 BcdHours, BcdMinutes, BcdSeconds;
     NvU32 data = 0;
+    NvU8 BcdDD, BcdMM, BcdYY1, BcdYY2;
+    NvU16 YYYY;
+    struct rtc_time tm;
+    NvU32 data1;
 
-    BcdHours = BcdMinutes = BcdSeconds = 0;
+    NVODMPMU_PRINTF(("\n Rtc write count=0x%x ", Count));
+    // convert seconds since reference time into date
+    // NOTE: using linux specific convert function rtc_time_to_tm
+    rtc_time_to_tm(Count, &tm);
+    NVODMPMU_PRINTF(("\n rtc_time_to_tm: YYYY=%d MM=%d DD=%d Hr=%d Min=%d "
+        "Sec=%d, *Count=0x%x ", (tm.tm_year + LINUX_RTC_BASE_YEAR),
+        (tm.tm_mon + 1), tm.tm_mday,
+        tm.tm_hour, tm.tm_min, tm.tm_sec, Count));
 
-    RtcDays = Count / MAX8907B_SECONDS_PER_DAY;
-
-    Hours   = (Count % MAX8907B_SECONDS_PER_DAY) / MAX8907B_SECONDS_PER_HOUR;
-    Minutes = ((Count % MAX8907B_SECONDS_PER_DAY) % MAX8907B_SECONDS_PER_HOUR) / MAX8907B_SECONDS_PER_MINUTE;
-    Seconds = Count % MAX8907B_SECONDS_PER_MINUTE;
-
-    BcdHours   = ((  Hours/10) << 4) | (  Hours%10);
-    BcdMinutes = ((Minutes/10) << 4) | (Minutes%10);
-    BcdSeconds = ((Seconds/10) << 4) | (Seconds%10);
+    // Convert time to bcd format
+    BcdHours   = ((tm.tm_hour / 10) << 4) | (tm.tm_hour % 10);
+    BcdMinutes = ((tm.tm_min/10) << 4) | (tm.tm_min%10);
+    BcdSeconds = ((tm.tm_sec/10) << 4) | (tm.tm_sec%10);
 
     data = (BcdSeconds << 24) | (BcdMinutes << 16) | (BcdHours << 8);
+    // write time - seconds, minutes and hours in a day to RTC registers
     if (Max8907bRtcI2cWriteTime(hDevice, MAX8907B_RTC_SEC, data))
     {
-        bRtcNotInitialized = NV_FALSE;
-        return NV_TRUE;
+        // set the day, month, year
+        // Assuming we get the days since 1 Jan 1970
+
+        // convert date to bct format
+        BcdDD = (((NvU8)tm.tm_mday / 10) << 4) | ((NvU8)tm.tm_mday % 10);
+        BcdMM = (((NvU8)tm.tm_mon / 10) << 4) | ((NvU8)tm.tm_mon % 10);
+        YYYY = (NvU16)tm.tm_year + LINUX_RTC_BASE_YEAR;
+        BcdYY1 = (((NvU8)(YYYY % 100) / 10) << 4) | ((NvU8)(YYYY % 100) % 10);
+        BcdYY2 = (((NvU8)(YYYY / 100) / 10) << 4) | ((NvU8)(YYYY / 100) % 10);
+        data = (NvU32)((BcdDD << 24) | (BcdMM << 16) | (BcdYY1 << 8) | BcdYY2);
+        // write date - day, month, and year to RTC registers
+        if (!(Max8907bRtcI2cWriteTime(hDevice, MAX8907B_RTC_DATE, data)))
+        {
+            NVODMPMU_PRINTF(("\n Max8907bRtcCountWrite() error. "));
+            return NV_FALSE;
+        }
+#if NV_DEBUG
+        // verify that read back values from RTC matches written values
+        if (!(Max8907bRtcI2cReadTime(hDevice, MAX8907B_RTC_DATE, &data1)))
+        {
+            NVODMPMU_PRINTF(("\n Max8907bRtcCountRead() error. "));
+            return NV_FALSE;
+        }
+        if (data1 == data)
+        {
+            NVODMPMU_PRINTF(("\n Write read Success. "));
+            return NV_TRUE;
+        }
+        else
+        {
+            // return error when read data does not match written data
+            NVODMPMU_PRINTF(("\n Error: write data=0x%x, rd data=0x%x. ", data, data1));
+            return NV_FALSE;
+        }
+#endif
     }
     else
     {
-        NvOdmOsDebugPrintf("Max8907bRtcCountWrite() error. ");
+        NVODMPMU_PRINTF(("\n Max8907bRtcCountWrite() error. "));
         return NV_FALSE;
     }
 }
 
-NvBool 
+NvBool
 Max8907bRtcAlarmCountWrite(
-    NvOdmPmuDeviceHandle hDevice, 
+    NvOdmPmuDeviceHandle hDevice,
     NvU32 Count)
 {
     return NV_FALSE;
 }
 
-NvBool 
+NvBool
 Max8907bRtcIsAlarmIntEnabled(NvOdmPmuDeviceHandle hDevice)
 {
     return NV_FALSE;
 }
 
-NvBool 
+NvBool
 Max8907bRtcAlarmIntEnable(
-    NvOdmPmuDeviceHandle hDevice, 
+    NvOdmPmuDeviceHandle hDevice,
     NvBool Enable)
 {
     return NV_FALSE;
-}
-
-NvBool 
-Max8907bRtcWasStartUpFromNoPower(NvOdmPmuDeviceHandle hDevice)
-{
-    return NV_TRUE;
 }
 
 NvBool
--- arch/arm/mach-tegra/odm_kit/adaptations/pmu/tps6586x/nvodm_pmu_tps6586x_rtc.c
+++ arch/arm/mach-tegra/odm_kit/adaptations/pmu/tps6586x/nvodm_pmu_tps6586x_rtc.c
@@ -30,9 +30,19 @@
  *
  */
 
+#include <linux/time.h>
 #include "nvodm_pmu_tps6586x_rtc.h"
 #include "nvodm_pmu_tps6586x_i2c.h"
 #include "tps6586x_reg.h"
+
+// macro SHIFT_TO_2009 if 1, uses 2009 as reference year instead of 1970
+// This is because RTC in PMU TPS6586x can store duration of 34 years,
+// else we cannot retain date beyond 2004
+#define SHIFT_TO_2009 1
+#if SHIFT_TO_2009
+static unsigned long epoch = 2009;
+static unsigned long epoch_sec = 0;
+#endif
 
 static NvBool bRtcNotInitialized = NV_TRUE;
 
@@ -43,12 +53,12 @@ Tps6586xRtcCountRead(
     NvU32* Count)
 {
     NvU32 ReadBuffer[2];
-    
+
     // 1) The I2C address pointer must not be left pointing in the range 0xC6 to 0xCA
     // 2) The maximum time for the address pointer to be in this range is 1ms
     // 3) Always read RTC_ALARM2 in the following order to prevent the address pointer
-    // from stopping at 0xC6: RTC_ALARM2_LO, then RTC_ALARM2_HI        
-    
+    // from stopping at 0xC6: RTC_ALARM2_LO, then RTC_ALARM2_HI
+
     if (Tps6586xRtcWasStartUpFromNoPower(hDevice) && bRtcNotInitialized)
     {
         Tps6586xRtcCountWrite(hDevice, 0);
@@ -59,55 +69,76 @@ Tps6586xRtcCountRead(
         // The unit of the RTC count is second!!! 1024 tick = 1s.
         // Read all 40 bit and right move 10 = Read the hightest 32bit and right move 2
         Tps6586xI2cRead32(hDevice, TPS6586x_RC6_RTC_COUNT4, &ReadBuffer[0]);
-               
+
         Tps6586xI2cRead8(hDevice, TPS6586x_RCA_RTC_COUNT0, &ReadBuffer[1]);
-        
+
         Tps6586xI2cRead8(hDevice, TPS6586x_RC0_RTC_CTRL, &ReadBuffer[1]);
-        
+
         // return second
         *Count = ReadBuffer[0]>>2;
     }
-  
+#if SHIFT_TO_2009
+    // calculate epoch_sec once
+    if (!epoch_sec)
+        epoch_sec = mktime(epoch,1,1,0,0,0);
+    *Count += epoch_sec;
+#endif
+
     return NV_TRUE;
 }
 
 /* Write RTC count register */
 
-NvBool 
+NvBool
 Tps6586xRtcCountWrite(
-    NvOdmPmuDeviceHandle hDevice, 
+    NvOdmPmuDeviceHandle hDevice,
     NvU32 Count)
 {
     NvU32 ReadBuffer = 0;
-    
+#if SHIFT_TO_2009
+    // calculate epoch_sec once
+    if (!epoch_sec)
+        epoch_sec = mktime(epoch,1,1,0,0,0);
+    if (Count < (NvU32)epoch_sec)
+    {
+        // prevent setting date earlier than 'epoch'
+        pr_warning("\n Date being set cannot be earlier than least year=%d. "
+            "Setting as least year. ", (int)epoch);
+        // base year seconds count is 0
+        Count = 0;
+    }
+    else
+        Count -= (NvU32)epoch_sec;
+#endif
+
     // To enable incrementing of the RTC_COUNT[39:0] from an initial value set by the host,
-    // the RTC_ENABLE bit should be written to 1 only after the RTC_OUT voltage reaches 
+    // the RTC_ENABLE bit should be written to 1 only after the RTC_OUT voltage reaches
     // the operating range
-    
+
     // Clear RTC_ENABLE before writing RTC_COUNT
     Tps6586xI2cRead8(hDevice, TPS6586x_RC0_RTC_CTRL, &ReadBuffer);
     ReadBuffer = ReadBuffer & 0xDF;
     Tps6586xI2cWrite8(hDevice, TPS6586x_RC0_RTC_CTRL, ReadBuffer);
-        
+
     Tps6586xI2cWrite32(hDevice, TPS6586x_RC6_RTC_COUNT4, (Count<<2));
-    Tps6586xI2cWrite8(hDevice,  TPS6586x_RCA_RTC_COUNT0, 0);       
-    
+    Tps6586xI2cWrite8(hDevice,  TPS6586x_RCA_RTC_COUNT0, 0);
+
     // Set RTC_ENABLE after writing RTC_COUNT
     Tps6586xI2cRead8(hDevice, TPS6586x_RC0_RTC_CTRL, &ReadBuffer);
     ReadBuffer = ReadBuffer | 0x20;
     Tps6586xI2cWrite8(hDevice, TPS6586x_RC0_RTC_CTRL, ReadBuffer);
-    
+
     if (bRtcNotInitialized)
         bRtcNotInitialized = NV_FALSE;
-    
+
     return NV_TRUE;
 }
 
 /* Read RTC alarm count register */
 
-NvBool 
+NvBool
 Tps6586xRtcAlarmCountRead(
-    NvOdmPmuDeviceHandle hDevice, 
+    NvOdmPmuDeviceHandle hDevice,
     NvU32* Count)
 {
     return NV_FALSE;
@@ -115,9 +146,9 @@ Tps6586xRtcAlarmCountRead(
 
 /* Write RTC alarm count register */
 
-NvBool 
+NvBool
 Tps6586xRtcAlarmCountWrite(
-    NvOdmPmuDeviceHandle hDevice, 
+    NvOdmPmuDeviceHandle hDevice,
     NvU32 Count)
 {
     return NV_FALSE;
@@ -125,7 +156,7 @@ Tps6586xRtcAlarmCountWrite(
 
 /* Reads RTC alarm interrupt mask status */
 
-NvBool 
+NvBool
 Tps6586xRtcIsAlarmIntEnabled(NvOdmPmuDeviceHandle hDevice)
 {
     return NV_FALSE;
@@ -133,9 +164,9 @@ Tps6586xRtcIsAlarmIntEnabled(NvOdmPmuDeviceHandle hDevice)
 
 /* Enables / Disables the RTC alarm interrupt */
 
-NvBool 
+NvBool
 Tps6586xRtcAlarmIntEnable(
-    NvOdmPmuDeviceHandle hDevice, 
+    NvOdmPmuDeviceHandle hDevice,
     NvBool Enable)
 {
     return NV_FALSE;
@@ -143,7 +174,7 @@ Tps6586xRtcAlarmIntEnable(
 
 /* Checks if boot was from nopower / powered state */
 
-NvBool 
+NvBool
 Tps6586xRtcWasStartUpFromNoPower(NvOdmPmuDeviceHandle hDevice)
 {
     NvU32 Data = 0;
