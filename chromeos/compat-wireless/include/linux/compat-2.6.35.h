b49c6f6a10b538084dcfd6b4748c8fe69a34163d
Sam Leffler <sleffler@google.com>
2010-06-24 10:34:07 -0700

    Update to compat-wireless 2.6.34 (release); notable changes: - 2-3 mac80211 fixes - several iwlwifi bug fixes
    TEST=matfunc+secmat suites run on cros-{1,3,5}
    BUG=3946
    
    Review URL: http://codereview.chromium.org/2865016

--- chromeos/compat-wireless/include/linux/compat-2.6.35.h
+++ chromeos/compat-wireless/include/linux/compat-2.6.35.h
@@ -4,9 +4,24 @@
 #include <linux/version.h>
 
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,35))
+#include <linux/etherdevice.h>
+#include <net/sock.h>
 
 #define IW_HANDLER(id, func)			\
 	[IW_IOCTL_IDX(id)] = func
+
+#define  SDIO_BUS_ECSI		0x20	/* Enable continuous SPI interrupt */
+#define  SDIO_BUS_SCSI		0x40	/* Support continuous SPI interrupt */
+
+#define netdev_hw_addr dev_mc_list
+
+static inline wait_queue_head_t *sk_sleep(struct sock *sk)
+{
+	return sk->sk_sleep;
+}
+
+#define usb_alloc_coherent(dev, size, mem_flags, dma) usb_buffer_alloc(dev, size, mem_flags, dma)
+#define usb_free_coherent(dev, size, addr, dma) usb_buffer_free(dev, size, addr, dma)
 
 #endif /* (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,35)) */
 
