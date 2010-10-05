36c7b0f82df8158ec8265267ef46f89267e55c57
Dmitry Torokhov <dmitry.torokhov@gmail.com>
2010-10-04 21:46:10 -0700

    UPSTREAM: Input: synaptics - simplify pass-through port handling
    There was too much knowledge about internals if serio in the pass-through
    handling, clean it up.
    
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>
    (cherry picked from commit a8b3c0f57beaba9035e5339175628b63e551b243)
    Signed-off-by: Mandeep Singh Baines <msb@chromium.org>
    Change-Id: Ib9922f9ee05c8528d08aa13f6a2d938a3f2f407e
    
    Review URL: http://codereview.chromium.org/6028006

--- drivers/input/mouse/synaptics.h
+++ drivers/input/mouse/synaptics.h
@@ -110,6 +110,8 @@ struct synaptics_data {
 	unsigned char pkt_type;			/* packet type - old, new, etc */
 	unsigned char mode;			/* current mode byte */
 	int scroll;
+
+	struct serio *pt_port;			/* Pass-through serio port */
 };
 
 int synaptics_detect(struct psmouse *psmouse, bool set_properties);
