4e0e76a9e69094ed5af62e5f31a2cc4af0adb3da
Shawn Nematbakhsh <shawnn@google.com>
2012-12-13 19:28:02 -0800

    CHROMIUM: atkbd: Fix scancode handling on reconnect / resume from suspend.
    On resume from suspend there is a possibility for multi-byte scancodes
    to be handled incorrectly. atkbd_reconnect disables the processing of
    scancodes in software by calling atkbd_disable, but the keyboard may
    still be active because no disconnect command was sent. Later, software
    handling is re-enabled. If a multi-byte scancode sent from the keyboard
    straddles the re-enable, only the latter byte(s) will be handled.
    
    In practice, this leads to cases where multi-byte break codes (ex. "e0
    4d" - break code for right-arrow) are misread as make codes ("4d" - make
    code for numeric 6), leading to one or more unwanted, untyped characters
    being interpreted.
    
    The solution implemented here involves sending command f5 (reset
    disable) to the keyboard prior to disabling software handling of codes.
    Later, the command to re-enable the keyboard is sent only after we are
    prepared to handle scancodes.
    
    TEST=spam right/left arrow keys during resume for many iterations,
    verify that no phantom characters are seen.
    BUG=chrome-os-partner:16605
    
    Change-Id: Ieba1e42398c2f4f6a1623afa3ed82903dc393045
    Signed-off-by: Shawn Nematbakhsh <shawnn@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/39690
    Reviewed-by: Derek Basehore <dbasehore@chromium.org>
    Reviewed-by: Andrew de los Reyes <adlr@chromium.org>

--- drivers/input/keyboard/atkbd.c
+++ drivers/input/keyboard/atkbd.c
@@ -1228,6 +1228,9 @@ static int atkbd_reconnect(struct serio *serio)
 
 	mutex_lock(&atkbd->mutex);
 
+	if (atkbd->write)
+		atkbd_deactivate(atkbd);
+
 	atkbd_disable(atkbd);
 
 	if (atkbd->write) {
