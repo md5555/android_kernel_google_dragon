909de5790627cb8099ab93e81a51306d113135dc
Allen Martin <amartin@nvidia.com>
2014-06-02 13:47:53 +0300

    CHROMIUM: ion: add dmabuf_is_ion()
    Add utility func, mainly for debug() purpose.
    
    Change-Id: I2fd24374f67e062aebd50384dceb99e0eac5f49f
    Reviewed-on: http://git-master/r/417721
    Signed-off-by: Lauri Peltonen <lpeltonen@nvidia.com>
    Reviewed-on: http://git-master/r/654170
    Reviewed-by: Hiroshi Doyu <hdoyu@nvidia.com>
    Signed-off-by: Allen Martin <amartin@nvidia.com>
    (cherry picked from commit 66bb24e0be1a3144534a34e584acb43a6903b519)
    Signed-off-by: Stéphane Marchesin <marcheu@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/269886

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -1211,6 +1211,12 @@ static struct dma_buf_ops dma_buf_ops = {
 	.vunmap = ion_dma_buf_vunmap,
 };
 
+bool dmabuf_is_ion(struct dma_buf *dmabuf)
+{
+	return dmabuf->ops == &dma_buf_ops;
+}
+EXPORT_SYMBOL(dmabuf_is_ion);
+
 struct dma_buf *ion_share_dma_buf(struct ion_client *client,
 						struct ion_handle *handle)
 {
@@ -1269,7 +1275,7 @@ struct ion_handle *ion_import_dma_buf(struct ion_client *client, int fd)
 		return ERR_CAST(dmabuf);
 	/* if this memory came from ion */
 
-	if (dmabuf->ops != &dma_buf_ops) {
+	if (dmabuf_is_ion(dmabuf)) {
 		pr_err("%s: can not import dmabuf from another exporter\n",
 		       __func__);
 		dma_buf_put(dmabuf);
@@ -1315,7 +1321,7 @@ static int ion_sync_for_device(struct ion_client *client, int fd)
 		return PTR_ERR(dmabuf);
 
 	/* if this memory came from ion */
-	if (dmabuf->ops != &dma_buf_ops) {
+	if (!dmabuf_is_ion(dmabuf)) {
 		pr_err("%s: can not sync dmabuf from another exporter\n",
 		       __func__);
 		dma_buf_put(dmabuf);
