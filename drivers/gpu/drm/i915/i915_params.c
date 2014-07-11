57a6ea0c7ab77746d4dc6e939aed28c881c715a7
Rodrigo Vivi <rodrigo.vivi@intel.com>
2014-07-11 10:30:19 -0700

    UPSTREAM: drm/i915: Enable PSR by default.
    Panel Self Refresh is an eDP power saving feature specified by VESA's eDP v1.3,
    that allows some panel componets to shutdown while you still see static images on
    the screen. Besides being supported on the platform it must be supported by the
    eDP panel itself.
    
    Now that we have the propper frontbuffer tracking support and correct locks on place
    we can enabled this feature by default.
    
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    (cherry picked from commit b6d547791fd3ef4ccc89ad2556ab01045640aef7)
    Signed-off-by: Wayne Boyer <wayne.boyer@intel.com>

--- drivers/gpu/drm/i915/i915_params.c
+++ drivers/gpu/drm/i915/i915_params.c
@@ -38,7 +38,7 @@ struct i915_params i915 __read_mostly = {
 	.enable_execlists = 0,
 	.enable_hangcheck = true,
 	.enable_ppgtt = -1,
-	.enable_psr = 0,
+	.enable_psr = 1,
 	.preliminary_hw_support = IS_ENABLED(CONFIG_DRM_I915_PRELIMINARY_HW_SUPPORT),
 	.disable_power_well = 1,
 	.enable_ips = 1,
@@ -125,7 +125,7 @@ MODULE_PARM_DESC(enable_execlists,
 	"(-1=auto, 0=disabled [default], 1=enabled)");
 
 module_param_named(enable_psr, i915.enable_psr, int, 0600);
-MODULE_PARM_DESC(enable_psr, "Enable PSR (default: false)");
+MODULE_PARM_DESC(enable_psr, "Enable PSR (default: true)");
 
 module_param_named(preliminary_hw_support, i915.preliminary_hw_support, int, 0600);
 MODULE_PARM_DESC(preliminary_hw_support,
