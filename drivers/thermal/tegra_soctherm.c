4d31ae575967f17311772accfccd0a321d8b3a3f
Wei Ni <wni@nvidia.com>
2014-08-20 10:45:15 +0800

    CHROMIUM: thermal: tegra-soctherm: remove unneccessary config parameter
    Remove unneccessary config parameter.
    
    BUG=chrome-os-partner:30834
    TEST=build successful.
    
    Change-Id: Iebaef13cab323996ea5d200f746222196a8fc00b
    Signed-off-by: Wei Ni <wni@nvidia.com>
    Reviewed-on: https://chromium-review.googlesource.com/216629
    Reviewed-by: Olof Johansson <olofj@chromium.org>
    Commit-Queue: Olof Johansson <olofj@chromium.org>
    Tested-by: Olof Johansson <olofj@chromium.org>
    Reviewed-on: https://chrome-internal-review.googlesource.com/210470
    Reviewed-by: Dylan Reid <dgreid@chromium.org>
    Tested-by: Dylan Reid <dgreid@chromium.org>
    Commit-Queue: Dylan Reid <dgreid@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/267525
    Tested-by: David Riley <davidriley@chromium.org>
    Reviewed-by: David Riley <davidriley@chromium.org>
    Commit-Queue: David Riley <davidriley@chromium.org>

--- drivers/thermal/tegra_soctherm.c
+++ drivers/thermal/tegra_soctherm.c
@@ -494,7 +494,6 @@ static const struct thermal_zone_of_device_ops tegra_of_thermal_ops = {
 };
 
 int tegra_soctherm_probe(struct platform_device *pdev,
-		struct tegra_tsensor_configuration *tegra_tsensor_configs,
 		struct tegra_tsensor *tsensors,
 		struct tegra_tsensor_group **tegra_tsensor_groups,
 		u8 nominal_calib_cp, u8 nominal_calib_ft)
