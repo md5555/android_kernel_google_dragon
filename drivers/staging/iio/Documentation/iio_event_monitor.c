2d4f164b3a092651dbacc67563159ab8c463f112
Daniel Baluta <daniel.baluta@intel.com>
2014-11-10 14:45:35 +0200

    BACKPORT: iio: event_monitor: Add support for new channel types
    We have the following testing scenario:
    
    $ insmod iio_dummy_evgen.ko
    $ insmod iio_dummy.ko
    
    ./iio_event_monitor /dev/iio:device0
    Event: time: 1412786467971335337, type: activity(running), channel: 0,
    evtype: thresh, direction: rising
    Event: time: 1412786530792974091, type: activity(walking), channel: 0,
    evtype: thresh, direction: falling
    Event: time: 1412764319184761765, type: steps, channel: 0, evtype: instance
    
    $ echo 1 > /sys/bus/iio/devices/iio_evgen/poke_ev0
    $ echo 2 > /sys/bus/iio/devices/iio_evgen/poke_ev0
    $ echo 3 > /sys/bus/iio/devices/iio_evgen/poke_ev0
    
    BUG=b:23570481
    TEST=compile
    
    Signed-off-by: Irina Tirdea <irina.tirdea@intel.com>
    Signed-off-by: Daniel Baluta <daniel.baluta@intel.com>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>
    (cherry picked from commit 282a5663930ba79af9ec38884580c140fedf8aaa)
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
    
    Change-Id: I63de8207bcf46a562a5a436768e03336e9d13558
    Reviewed-on: https://chromium-review.googlesource.com/298268
    Commit-Ready: Gwendal Grignou <gwendal@chromium.org>
    Tested-by: Gwendal Grignou <gwendal@chromium.org>
    Reviewed-by: Benson Leung <bleung@chromium.org>

--- drivers/staging/iio/Documentation/iio_event_monitor.c
+++ drivers/staging/iio/Documentation/iio_event_monitor.c
@@ -49,6 +49,8 @@ static const char * const iio_chan_type_name_spec[] = {
 	[IIO_CCT] = "cct",
 	[IIO_PRESSURE] = "pressure",
 	[IIO_HUMIDITYRELATIVE] = "humidityrelative",
+	[IIO_ACTIVITY] = "activity",
+	[IIO_STEPS] = "steps",
 };
 
 static const char * const iio_ev_type_text[] = {
@@ -57,6 +59,7 @@ static const char * const iio_ev_type_text[] = {
 	[IIO_EV_TYPE_ROC] = "roc",
 	[IIO_EV_TYPE_THRESH_ADAPTIVE] = "thresh_adaptive",
 	[IIO_EV_TYPE_MAG_ADAPTIVE] = "mag_adaptive",
+	[IIO_EV_TYPE_INSTANCE] = "instance",
 };
 
 static const char * const iio_ev_dir_text[] = {
@@ -79,6 +82,10 @@ static const char * const iio_modifier_names[] = {
 	[IIO_MOD_LIGHT_RED] = "red",
 	[IIO_MOD_LIGHT_GREEN] = "green",
 	[IIO_MOD_LIGHT_BLUE] = "blue",
+	[IIO_MOD_RUNNING] = "running",
+	[IIO_MOD_JOGGING] = "jogging",
+	[IIO_MOD_WALKING] = "walking",
+	[IIO_MOD_STILL] = "still",
 };
 
 static bool event_is_known(struct iio_event_data *event)
@@ -108,6 +115,8 @@ static bool event_is_known(struct iio_event_data *event)
 	case IIO_CCT:
 	case IIO_PRESSURE:
 	case IIO_HUMIDITYRELATIVE:
+	case IIO_ACTIVITY:
+	case IIO_STEPS:
 		break;
 	default:
 		return false;
@@ -126,6 +135,10 @@ static bool event_is_known(struct iio_event_data *event)
 	case IIO_MOD_LIGHT_RED:
 	case IIO_MOD_LIGHT_GREEN:
 	case IIO_MOD_LIGHT_BLUE:
+	case IIO_MOD_RUNNING:
+	case IIO_MOD_JOGGING:
+	case IIO_MOD_WALKING:
+	case IIO_MOD_STILL:
 		break;
 	default:
 		return false;
@@ -137,6 +150,7 @@ static bool event_is_known(struct iio_event_data *event)
 	case IIO_EV_TYPE_ROC:
 	case IIO_EV_TYPE_THRESH_ADAPTIVE:
 	case IIO_EV_TYPE_MAG_ADAPTIVE:
+	case IIO_EV_TYPE_INSTANCE:
 		break;
 	default:
 		return false;
@@ -146,6 +160,7 @@ static bool event_is_known(struct iio_event_data *event)
 	case IIO_EV_DIR_EITHER:
 	case IIO_EV_DIR_RISING:
 	case IIO_EV_DIR_FALLING:
+	case IIO_EV_DIR_NONE:
 		break;
 	default:
 		return false;
@@ -186,9 +201,11 @@ static void print_event(struct iio_event_data *event)
 	else if (chan >= 0)
 		printf("channel: %d, ", chan);
 
-	printf("evtype: %s, direction: %s\n",
-		iio_ev_type_text[ev_type],
-		iio_ev_dir_text[dir]);
+	printf("evtype: %s", iio_ev_type_text[ev_type]);
+
+	if (dir != IIO_EV_DIR_NONE)
+		printf(", direction: %s", iio_ev_dir_text[dir]);
+	printf("\n");
 }
 
 int main(int argc, char **argv)
