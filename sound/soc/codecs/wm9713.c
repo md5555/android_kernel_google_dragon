c01c61d12f96997767ea7610b2d045de04767ff3
Lars-Peter Clausen <lars@metafoo.de>
2014-11-23 13:37:48 +0100

    UPSTREAM: ASoC: wm9713: Cleanup manual bias level transitions
    The manual transition to SND_SOC_BIAS_STANDBY at the end of CODEC probe()
    can also be removed as the core will automatically do this after the CODEC
    has been probed.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Acked-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit 0cb6b1419ec864996835991a62788c588693f27d)
    Signed-off-by: Nicolas Boichat <drinkcat@chromium.org>
    
    BUG=chromium:534241
    TEST=x86-64 allmodconfig
    
    Change-Id: Ie33260728c75909518aaf061931acd3685324f23
    Reviewed-on: https://chromium-review.googlesource.com/301049
    Commit-Ready: Nicolas Boichat <drinkcat@chromium.org>
    Tested-by: Nicolas Boichat <drinkcat@chromium.org>
    Reviewed-by: Ben Zhang <benzh@chromium.org>

--- sound/soc/codecs/wm9713.c
+++ sound/soc/codecs/wm9713.c
@@ -1210,8 +1210,6 @@ static int wm9713_soc_probe(struct snd_soc_codec *codec)
 		goto reset_err;
 	}
 
-	wm9713_set_bias_level(codec, SND_SOC_BIAS_STANDBY);
-
 	/* unmute the adc - move to kcontrol */
 	reg = ac97_read(codec, AC97_CD) & 0x7fff;
 	ac97_write(codec, AC97_CD, reg);
