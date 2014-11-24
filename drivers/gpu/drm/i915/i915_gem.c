8007059384ed40e6365d3f2d17ae83c089e6085b
John Harrison <John.C.Harrison@Intel.com>
2014-11-24 18:49:36 +0000

    drm/i915: Remove obsolete seqno parameter from 'i915_add_request'
    There is no longer any need to retrieve a seqno value from an i915_add_request()
    call. The calling code already knows which request structure is being processed
    (it can only be ring->OLR). And as the request itself is now used in preference
    to the basic seqno value, the latter is now redundant in this situation.
    
    For: VIZ-4377
    Signed-off-by: John Harrison <John.C.Harrison@Intel.com>
    Reviewed-by: Thomas Daniel <Thomas.Daniel@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    (cherry picked from commit 9400ae5c8248dd29c55f2c2355b71e55995774d3)
    Signed-off-by: Marc Herbert <marc.herbert@intel.com>

--- drivers/gpu/drm/i915/i915_gem.c
+++ drivers/gpu/drm/i915/i915_gem.c
@@ -1168,7 +1168,7 @@ i915_gem_check_olr(struct drm_i915_gem_request *req)
 
 	ret = 0;
 	if (req == req->ring->outstanding_lazy_request)
-		ret = i915_add_request(req->ring, NULL);
+		ret = i915_add_request(req->ring);
 
 	return ret;
 }
@@ -2411,8 +2411,7 @@ i915_gem_get_seqno(struct drm_device *dev, u32 *seqno)
 
 int __i915_add_request(struct intel_engine_cs *ring,
 		       struct drm_file *file,
-		       struct drm_i915_gem_object *obj,
-		       u32 *out_seqno)
+		       struct drm_i915_gem_object *obj)
 {
 	struct drm_i915_private *dev_priv = ring->dev->dev_private;
 	struct drm_i915_gem_request *request;
@@ -2511,8 +2510,6 @@ int __i915_add_request(struct intel_engine_cs *ring,
 			   round_jiffies_up_relative(HZ));
 	intel_mark_busy(dev_priv->dev);
 
-	if (out_seqno)
-		*out_seqno = request->seqno;
 	return 0;
 }
 
