93101f629ab5ea734c221e768216d033a305749b
Viresh Kumar <viresh.kumar@linaro.org>
2014-11-27 08:54:06 +0530

    BACKPORT: PM / OPP Introduce APIs to remove OPPs
    OPPs are created statically (from DT) or dynamically. Currently we don't free
    OPPs that are created statically, when the module unloads. And so if the module
    is inserted back again, we get warning for duplicate OPPs as the same were
    already present.
    
    Also, there might be a need to remove dynamic OPPs in future and so API for that
    is also added.
    
    This patch adds helper APIs to remove/free existing static and dynamic OPPs.
    
    Because the OPPs are used both under RCU and SRCU, we have to wait for grace
    period of both. And so are using kfree_rcu() from within call_srcu().
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    (cherry picked from commit 129eec55df6ab1b5ecdd89fd7db7a2cd103200b5)
    Signed-off-by: Pi-Cheng Chen <pi-cheng.chen@mediatek.com>
    
    [pi-cheng.chen: this patch has a trivial conflict with
     chromeos-3.18 commit [0] which added of_init_opp_table_named()
     and converted of_init_opp_table() to call it.
     [0] 6aff17c221456a710682af44a42147a8e60a96a9
     CHROMIUM: PM / OPP: introduce of_init_opp_table_named
    ]
    
    BUG=chrome-os-partner:39501
    TEST=build and boot oak-rev2 and test each OPP of two clusters
    
    Change-Id: Ib3730bf6c90fbc9faee219f9799ab0a2f1480c62
    Reviewed-on: https://chromium-review.googlesource.com/278130
    Reviewed-by: Daniel Kurtz <djkurtz@chromium.org>
    Tested-by: Daniel Kurtz <djkurtz@chromium.org>
    Commit-Queue: Daniel Kurtz <djkurtz@chromium.org>
    Trybot-Ready: Daniel Kurtz <djkurtz@chromium.org>

--- include/linux/pm_opp.h
+++ include/linux/pm_opp.h
@@ -21,7 +21,7 @@ struct dev_pm_opp;
 struct device;
 
 enum dev_pm_opp_event {
-	OPP_EVENT_ADD, OPP_EVENT_ENABLE, OPP_EVENT_DISABLE,
+	OPP_EVENT_ADD, OPP_EVENT_REMOVE, OPP_EVENT_ENABLE, OPP_EVENT_DISABLE,
 };
 
 #if defined(CONFIG_PM_OPP)
@@ -44,6 +44,7 @@ struct dev_pm_opp *dev_pm_opp_find_freq_ceil(struct device *dev,
 
 int dev_pm_opp_add(struct device *dev, unsigned long freq,
 		   unsigned long u_volt);
+void dev_pm_opp_remove(struct device *dev, unsigned long freq);
 
 int dev_pm_opp_enable(struct device *dev, unsigned long freq);
 
@@ -90,6 +91,10 @@ static inline int dev_pm_opp_add(struct device *dev, unsigned long freq,
 	return -EINVAL;
 }
 
+static inline void dev_pm_opp_remove(struct device *dev, unsigned long freq)
+{
+}
+
 static inline int dev_pm_opp_enable(struct device *dev, unsigned long freq)
 {
 	return 0;
@@ -114,6 +119,7 @@ static inline int of_init_opp_table(struct device *dev)
 {
 	return of_init_opp_table_named(dev, "operating-points");
 }
+void of_free_opp_table(struct device *dev);
 #else
 static inline int of_init_opp_table_named(struct device *dev, const char *name)
 {
@@ -122,6 +128,10 @@ static inline int of_init_opp_table_named(struct device *dev, const char *name)
 static inline int of_init_opp_table(struct device *dev)
 {
 	return -EINVAL;
+}
+
+static inline void of_free_opp_table(struct device *dev)
+{
 }
 #endif
 
