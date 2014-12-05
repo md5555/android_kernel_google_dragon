d3ecfe9ee738d3100d85adc6d2536a070e47085f
Adrian Hunter <adrian.hunter@intel.com>
2014-12-05 19:25:29 +0200

    UPSTREAM: mmc: sdhci: Add out_unlock to sdhci_execute_tuning
    A 'goto' can be used to save duplicating unlocking
    and returning.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    Reviewed-by: Aaron Lu <aaron.lu@intel.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    (cherry picked from commit d519c863fc198f43c979e9d6a74b53d9c53d55aa)
    Signed-off-by: Marc Herbert <marc.herbert@intel.com>
    
    BUG=chrome-os-partner:40410
    TEST=With this whole HS400 series applied:
    TEST=on a system with HS400 controller(s), it/they initialize with the
      following log message:
      INFO kernel: [  103.601747] mmc0: new HS400 MMC card at address 0001
    TEST=run SD/eMMC test plan: higher eMMC throughput after change, and no
      other change.
    TEST=same on another system with no HS400: no observable change at all.
    
    Change-Id: Ida7c4e3dfc311f6b872479fadae79ed07b17e947
    Reviewed-on: https://chromium-review.googlesource.com/276391
    Commit-Ready: Marc Herbert <marc.herbert@intel.com>
    Tested-by: Philip Hanson <philip.hanson@intel.com>
    Reviewed-by: Derek Basehore <dbasehore@chromium.org>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -1916,9 +1916,7 @@ static int sdhci_execute_tuning(struct mmc_host *mmc, u32 opcode)
 		/* FALLTHROUGH */
 
 	default:
-		spin_unlock_irqrestore(&host->lock, flags);
-		sdhci_runtime_pm_put(host);
-		return 0;
+		goto out_unlock;
 	}
 
 	if (host->ops->platform_execute_tuning) {
@@ -2102,6 +2100,7 @@ out:
 
 	sdhci_writel(host, host->ier, SDHCI_INT_ENABLE);
 	sdhci_writel(host, host->ier, SDHCI_SIGNAL_ENABLE);
+out_unlock:
 	spin_unlock_irqrestore(&host->lock, flags);
 	sdhci_runtime_pm_put(host);
 
