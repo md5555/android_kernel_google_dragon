e8834d053758d14b141dc2c41b874065feb2d838
Dmitry Torokhov <dtor@chromium.org>
2014-12-11 12:21:40 -0800

    CHROMIUM: cpufreq-cpu0: defer probing if OPP table is not ready
    cpufreq-cpu0 driver supports mode when OPP table is provided by platform
    code and not device tree. However on certain platforms code that fills
    OPP table may run after cpufreq driver tries to initialize, so let's
    report EPROBE_DEFER if we do not find any entires in OPP table for the
    CPU.
    
    BUG=chrome-os-partner:34104
    TEST=Build and boot.
    
    Change-Id: Id198b49f579745bc171dd63fed4556338fffdf1a
    Signed-off-by: Dmitry Torokhov <dtor@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/234845
    Reviewed-by: Doug Anderson <dianders@chromium.org>
    
    Conflicts:
    	drivers/cpufreq/cpufreq-cpu0.c
    
    [filbranden: 3.18 rebase. Moved from cpufreq-cpu0.c to cpufreq-dt.c, as
     upstream, also adjusted goto target to match the ones in the new code.]
    Signed-off-by: Filipe Brandenburger <filbranden@chromium.org>

--- drivers/cpufreq/cpufreq-dt.c
+++ drivers/cpufreq/cpufreq-dt.c
@@ -258,6 +258,15 @@ static int cpufreq_init(struct cpufreq_policy *policy)
 			transition_latency += ret * 1000;
 	}
 
+	rcu_read_lock();
+	ret = dev_pm_opp_get_opp_count(cpu_dev);
+	rcu_read_unlock();
+	if (ret <= 0) {
+		pr_debug("OPP table is not ready, deferring probe\n");
+		ret = -EPROBE_DEFER;
+		goto out_free_priv;
+	}
+
 	ret = dev_pm_opp_init_cpufreq_table(cpu_dev, &freq_table);
 	if (ret) {
 		pr_err("failed to init cpufreq table: %d\n", ret);
