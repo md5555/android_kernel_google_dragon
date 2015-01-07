e4987b0bf7a37bfb30d631b1b7d46e62d726b667
Marc Yang <yangyang@marvell.com>
2015-01-07 04:11:12 -0800

    UPSTREAM: mwifiex: Adjust calling place of mwifiex_terminate_workqueue
    Workqueue needs to be flushed early when removing card, otherwise
    soft lockup issue may happen because main_process is triggered by
    interrupt while card is being removed.
    
    Signed-off-by: Marc Yang <yangyang@marvell.com>
    Signed-off-by: Cathy Luo <cluo@marvell.com>
    Signed-off-by: Amitkumar Karwar <akarwar@marvell.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    
    BUG=chrome-os-partner:34110
    TEST=With all patches in this series applied, ran suspend_stress
    test to pass 10000 iterations on Lucas2
    
    Change-Id: Ic99927f9db2c9d478607a0b1a50de76c378d1f81
    Reviewed-on: https://chromium-review.googlesource.com/239145
    Reviewed-by: Paul Stewart <pstew@chromium.org>
    Tested-by: Amitkumar Karwar <akarwar@marvell.com>
    Commit-Queue: Amitkumar Karwar <akarwar@marvell.com>
    Reviewed-on: https://chromium-review.googlesource.com/313355
    Commit-Ready: Amitkumar Karwar <akarwar@marvell.com>
    Reviewed-by: Wei-Ning Huang <wnhuang@chromium.org>

--- drivers/net/wireless/mwifiex/main.c
+++ drivers/net/wireless/mwifiex/main.c
@@ -1015,6 +1015,8 @@ int mwifiex_remove_card(struct mwifiex_adapter *adapter, struct semaphore *sem)
 
 	adapter->surprise_removed = true;
 
+	mwifiex_terminate_workqueue(adapter);
+
 	/* Stop data */
 	for (i = 0; i < adapter->priv_num; i++) {
 		priv = adapter->priv[i];
@@ -1056,8 +1058,6 @@ int mwifiex_remove_card(struct mwifiex_adapter *adapter, struct semaphore *sem)
 
 	wiphy_unregister(adapter->wiphy);
 	wiphy_free(adapter->wiphy);
-
-	mwifiex_terminate_workqueue(adapter);
 
 	/* Unregister device */
 	dev_dbg(adapter->dev, "info: unregister device\n");
