3c4c37cea642d00b3f2d0ae650a80a8f09b0ceec
Morten Rasmussen <morten.rasmussen@arm.com>
2015-01-27 13:48:07 +0000

    CHROMIUM: sched, cpuidle: Track cpuidle state index in the scheduler
    The idle-state of each cpu is currently pointed to by rq->idle_state but
    there isn't any information in the struct cpuidle_state that can used to
    look up the idle-state energy model data stored in struct
    sched_group_energy. For this purpose is necessary to store the idle
    state index as well. Ideally, the idle-state data should be unified.
    
    BUG=chrome-os-partner:44828
    TEST=Boot kernel on Oak.
    
    cc: Ingo Molnar <mingo@redhat.com>
    cc: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Morten Rasmussen <morten.rasmussen@arm.com>
    
    Change-Id: Ib013757dd192b6f6f0be098b3909f42cf2fb723b
    Signed-off-by: Ricky Liang <jcliang@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/321290
    Reviewed-by: Andrew Bresticker <abrestic@chromium.org>
    (cherry picked from commit 4043a3161600cf253579a13cde5b102fffc74551)

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -162,7 +162,7 @@ int cpuidle_enter_state(struct cpuidle_device *dev, struct cpuidle_driver *drv,
 	s64 diff;
 
 	/* Take note of the planned idle state. */
-	sched_idle_set_state(target_state);
+	sched_idle_set_state(target_state, index);
 
 	trace_cpu_idle_rcuidle(index, dev->cpu);
 	time_start = ktime_get();
@@ -173,7 +173,7 @@ int cpuidle_enter_state(struct cpuidle_device *dev, struct cpuidle_driver *drv,
 	trace_cpu_idle_rcuidle(PWR_EVENT_EXIT, dev->cpu);
 
 	/* The cpu is no longer idle or about to enter idle. */
-	sched_idle_set_state(NULL);
+	sched_idle_set_state(NULL, -1);
 
 	if (!cpuidle_state_is_coupled(dev, drv, entered_state))
 		local_irq_enable();
