b018d703185a9543c5377c4365b6d1a2621d7596
Allen Martin <amartin@nvidia.com>
2015-02-05 14:33:35 +0800

    CHROMIUM: soc/tegra: move soc_device_register call out of arch/arm
    Change-Id: I50014eb3ec97761cce9a051a7b65d0f1d72dea15
    Signed-off-by: Joseph Lo <josephl@nvidia.com>
    Signed-off-by: Allen Martin <amartin@nvidia.com>
    Signed-off-by: David Riley <davidriley@chromium.org>
    Reviewed-on: https://chrome-internal-review.googlesource.com/209051
    Reviewed-by: David Riley <davidriley@google.com>
    Commit-Queue: David Riley <davidriley@google.com>
    Tested-by: David Riley <davidriley@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/267261

--- arch/arm/mach-tegra/tegra.c
+++ arch/arm/mach-tegra/tegra.c
@@ -85,36 +85,7 @@ static void __init tegra_dt_init_irq(void)
 
 static void __init tegra_dt_init(void)
 {
-	struct soc_device_attribute *soc_dev_attr;
-	struct soc_device *soc_dev;
-	struct device *parent = NULL;
-
-	soc_dev_attr = kzalloc(sizeof(*soc_dev_attr), GFP_KERNEL);
-	if (!soc_dev_attr)
-		goto out;
-
-	soc_dev_attr->family = kasprintf(GFP_KERNEL, "Tegra");
-	soc_dev_attr->revision = kasprintf(GFP_KERNEL, "%d",
-					   tegra_sku_info.revision);
-	soc_dev_attr->soc_id = kasprintf(GFP_KERNEL, "%u", tegra_get_chip_id());
-
-	soc_dev = soc_device_register(soc_dev_attr);
-	if (IS_ERR(soc_dev)) {
-		kfree(soc_dev_attr->family);
-		kfree(soc_dev_attr->revision);
-		kfree(soc_dev_attr->soc_id);
-		kfree(soc_dev_attr);
-		goto out;
-	}
-
-	parent = soc_device_to_device(soc_dev);
-
-	/*
-	 * Finished with the static registrations now; fill in the missing
-	 * devices
-	 */
-out:
-	of_platform_populate(NULL, of_default_bus_match_table, NULL, parent);
+	of_platform_populate(NULL, of_default_bus_match_table, NULL, NULL);
 }
 
 static void __init paz00_init(void)
--- drivers/soc/tegra/common.c
+++ drivers/soc/tegra/common.c
@@ -7,8 +7,11 @@
  */
 
 #include <linux/of.h>
+#include <linux/slab.h>
+#include <linux/sys_soc.h>
 
 #include <soc/tegra/common.h>
+#include <soc/tegra/fuse.h>
 
 static const struct of_device_id tegra_machine_match[] = {
 	{ .compatible = "nvidia,tegra20", },
@@ -29,3 +32,33 @@ bool soc_is_tegra(void)
 
 	return of_match_node(tegra_machine_match, root) != NULL;
 }
+
+static int __init tegra_soc_init(void)
+{
+	struct soc_device_attribute *soc_dev_attr;
+	struct soc_device *soc_dev;
+
+	if (!soc_is_tegra())
+		return 0;
+
+	soc_dev_attr = kzalloc(sizeof(*soc_dev_attr), GFP_KERNEL);
+	if (!soc_dev_attr)
+		return -ENOMEM;
+
+	soc_dev_attr->family = kasprintf(GFP_KERNEL, "Tegra");
+	soc_dev_attr->revision = kasprintf(GFP_KERNEL, "%d",
+					   tegra_sku_info.revision);
+	soc_dev_attr->soc_id = kasprintf(GFP_KERNEL, "%u", tegra_get_chip_id());
+
+	soc_dev = soc_device_register(soc_dev_attr);
+	if (IS_ERR(soc_dev)) {
+		kfree(soc_dev_attr->family);
+		kfree(soc_dev_attr->revision);
+		kfree(soc_dev_attr->soc_id);
+		kfree(soc_dev_attr);
+		return -ENODEV;
+	}
+
+	return 0;
+}
+device_initcall(tegra_soc_init);
