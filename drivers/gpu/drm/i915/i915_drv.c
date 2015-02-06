4f2c20be8b8bf65c4e5a1daf3bf27263f2af7b87
Andreas Ruprecht <rupran@einserver.de>
2015-02-06 11:16:47 +0100

    drm/i915: Remove references to previously removed UMS config option
    Commit 03dae59c72ffffd8 ("drm/i915: Ditch UMS config option") removed
    CONFIG_DRM_I915_UMS from the Kconfig file, but i915_drv.c still
    references this option in two #ifndef statements.
    
    As an undefined config option will always be 'false', we can drop
    the #ifndefs alltogether and adapt the printed error message.
    
    This inconsistency was found with the undertaker tool.
    
    Signed-off-by: Andreas Ruprecht <rupran@einserver.de>
    [danvet: Undo logging change as requested by Jani.]
    Reviewed-by: Jani Nikula <jani.nikula@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    
    (cherry picked from commit cd5956c9136d80ca09faacf9622dc68426bec98c)
    Signed-off-by: Marc Herbert <marc.herbert@intel.com>

--- drivers/gpu/drm/i915/i915_drv.c
+++ drivers/gpu/drm/i915/i915_drv.c
@@ -1645,11 +1645,9 @@ static int __init i915_init(void)
 
 	if (!(driver.driver_features & DRIVER_MODESET)) {
 		driver.get_vblank_timestamp = NULL;
-#ifndef CONFIG_DRM_I915_UMS
 		/* Silently fail loading to not upset userspace. */
 		DRM_DEBUG_DRIVER("KMS and UMS disabled.\n");
 		return 0;
-#endif
 	}
 
 	/*
@@ -1665,10 +1663,8 @@ static int __init i915_init(void)
 
 static void __exit i915_exit(void)
 {
-#ifndef CONFIG_DRM_I915_UMS
 	if (!(driver.driver_features & DRIVER_MODESET))
 		return; /* Never loaded a driver. */
-#endif
 
 	drm_pci_exit(&driver, &i915_pci_driver);
 }
