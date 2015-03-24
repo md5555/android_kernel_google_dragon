1ac5280ddf0bd549561769000ee9607e57175072
Hanjun Guo <hanjun.guo@linaro.org>
2015-03-24 14:02:56 +0000

    UPSTREAM: ARM64 / ACPI: Don't unflatten device tree if acpi=force is passed
    Since the policy is that once we pass acpi=force in the early
    param, we will not unflatten device tree even if ACPI is disabled
    in ACPI table init fails, so fix the code by comparinging both
    acpi_disabled and param_acpi_force before the device tree is
    unflattened.
    
    CC: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Signed-off-by: Hanjun Guo <hanjun.guo@linaro.org>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    (cherry picked from commit 33757ded074918eb49243968a82e7c9ec2d71720)
    Signed-off-by: Daniel Kurtz <djkurtz@chromium.org>
    
    BUG=chrome-os-partner:41584
    TEST=build and boot oak-rev2 & smaug w/ PSCI-v1.0 enabled FW image
    TEST=powerd_dbus_suspend ; resume using keyboard and touchpad
    
    Change-Id: Id2e4ed66b5fc93a5069c92d04ea09db6689bcba9
    Reviewed-on: https://chromium-review.googlesource.com/280453
    Reviewed-by: Benson Leung <bleung@chromium.org>
    Commit-Queue: Joseph Lo <josephl@nvidia.com>
    Tested-by: Joseph Lo <josephl@nvidia.com>

--- arch/arm64/kernel/setup.c
+++ arch/arm64/kernel/setup.c
@@ -409,7 +409,7 @@ void __init setup_arch(char **cmdline_p)
 	efi_virtmap_init();
 	early_ioremap_reset();
 
-	if (acpi_disabled) {
+	if (acpi_disabled  && !param_acpi_force) {
 		unflatten_device_tree();
 		psci_dt_init();
 		cpu_read_bootcpu_ops();
