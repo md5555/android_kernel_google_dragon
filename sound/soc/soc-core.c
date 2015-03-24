7f1dde1ea1a2911021eb79181b194b89618b8e0c
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2015-03-24 09:01:00 +0000

    UPSTREAM: ASoC: soc-core: initialize debugfs in snd_soc_instantiate_card()
    Current soc_init_card_debugfs() is called from snd_soc_register_card()
    but, soc_cleanup_card_debugfs() is called from soc_cleanup_card_resources(),
    not from paired function.
    
    This differences don't matter for now. But if anyone wants to implement
    a proper hotplug/unplug, this difference would become clearer.
    
    Now, we can assume that snd_soc_instantiate_card() and
    soc_cleanup_card_resources() are paired function.
    soc_init_card_debugfs() / soc_cleanup_card_debugfs() paired function
    should be called from these.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Link: git://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git
    br: master
    (cherry picked from commit 4e2576bd36a12e78ac3786d05b99a820dffe687f)
    Signed-off-by: Vedang Patel <vedang.patel@intel.com>
    
    BUG=None
    TEST=with the whole series applied, the audio subsystem is stable.
    TEST=there are no regressions observed on BSW.
    
    Change-Id: Ieb1170aec77da9b7eecf6b64c2201f8761e44fc2

--- sound/soc/soc-core.c
+++ sound/soc/soc-core.c
@@ -1726,6 +1726,8 @@ static int snd_soc_instantiate_card(struct snd_soc_card *card)
 	mutex_unlock(&card->mutex);
 	mutex_unlock(&client_mutex);
 
+	soc_init_card_debugfs(card);
+
 	return 0;
 
 probe_aux_dev_err:
@@ -2417,8 +2419,6 @@ int snd_soc_register_card(struct snd_soc_card *card)
 
 	snd_soc_initialize_card_lists(card);
 
-	soc_init_card_debugfs(card);
-
 	card->rtd = devm_kzalloc(card->dev,
 				 sizeof(struct snd_soc_pcm_runtime) *
 				 (card->num_links + card->num_aux_devs),
@@ -2449,7 +2449,7 @@ int snd_soc_register_card(struct snd_soc_card *card)
 
 	ret = snd_soc_instantiate_card(card);
 	if (ret != 0)
-		soc_cleanup_card_debugfs(card);
+		return ret;
 
 	/* deactivate pins to sleep state */
 	for (i = 0; i < card->num_rtd; i++) {
