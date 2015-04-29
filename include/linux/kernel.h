fe67f2bca153d87e5afa04666c0acb30511a837e
Daniel Kurtz <djkurtz@chromium.org>
2015-04-29 15:41:20 +0800

    Revert "CHROMIUM: Add CLOCK_SYSTEM_TRACE to export the trace clock to user space."
    This reverts commit e4b9aace45ea846dff86e45d55dacee1e87eddca.
    
    This patch was an incorrect rebase from chromoes-3.14 to chromeos-3.18.
    (it should be replacing CLOCK_TAI with CLOCK_SYSTEM_TRACE with ID 11 not
    incrementing it to 12).
    
    Furthermore, CLOCK_SYSTEM_TRACE is no longer required, so this patch can
    just be dropped.
    
    Signed-off-by: Daniel Kurtz <djkurtz@chromium.org>
    
    BUG=chromium:481177
    TEST=Build chromeos-3.18 w/ arm64 defconfig (w/ CONFIG_FTRACE=n):
     1. cd src/third_party/kernel/v3.18
     3. export ARCH=arm64
     4. export CROSS_COMPILE=aarch64-linux-gnu-
     5. make defconfig
     6. make -j32
     => No build breakage in include/linux/kernel.h
    
    Change-Id: I3fa472a69a74ba09fb362417256917e28f586c52
    Reviewed-on: https://chromium-review.googlesource.com/267644
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>
    Commit-Queue: Daniel Kurtz <djkurtz@chromium.org>
    Trybot-Ready: Daniel Kurtz <djkurtz@chromium.org>
    Tested-by: Daniel Kurtz <djkurtz@chromium.org>

--- include/linux/kernel.h
+++ include/linux/kernel.h
@@ -540,9 +540,6 @@ extern void tracing_start(void);
 extern void tracing_stop(void);
 extern void ftrace_cpu_off(void);
 extern void ftrace_cpu_on(void);
-struct timespec;
-extern void trace_clock_gettime(struct timespec *);
-extern void trace_clock_getres(struct timespec *);
 
 static inline __printf(1, 2)
 void ____trace_printk_check_format(const char *fmt, ...)
@@ -682,15 +679,6 @@ static inline void tracing_start(void) { }
 static inline void tracing_stop(void) { }
 static inline void ftrace_cpu_off(void) { }
 static inline void ftrace_cpu_on(void) { }
-struct timespec;
-static inline void trace_clock_gettime(struct timespec *tp)
-{
-	getrawmonotonic(tp);
-}
-static inline void trace_clock_getres(struct timespec *tp)
-{
-	*tp = ktime_to_timespec(KTIME_LOW_RES);
-}
 static inline void trace_dump_stack(int skip) { }
 
 static inline void tracing_on(void) { }
