a775019f7a16e5fd1385d54dba7a41dcc479f480
Peter Seiderer <ps.report@gmx.net>
2015-05-04 07:51:05 -0300

    UPSTREAM: [media] videodev2: Add V4L2_BUF_FLAG_LAST
    This v4l2_buffer flag can be used by drivers to mark a capture buffer
    as the last generated buffer, for example after a V4L2_DEC_CMD_STOP
    command was issued.
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>
    Acked-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Kamil Debski <k.debski@samsung.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>
    (cherry picked from commit dc199241624a2fd85d9b0d8303babd60feadd0e6)
    Signed-off-by: Daniel Kurtz <djkurtz@chromium.org>
    
    BUG=chrome-os-partner:38980
    TEST=build for oak and boot on oak-rev4 w/ v4l2 snapshot
    TEST=http://www.videojs.com/
    TEST=http://www.quirksmode.org/html5/tests/video.html
    TEST=https://www.youtube.com/watch?v=jblxjhgolc8
      => In each case, verify '0' increments on:
      chrome://histograms/Media.GpuVideoDecoderInitializeStatus
    
    Change-Id: Iee3d91218f07779586011f8bd19c4e9dea3297e4
    Reviewed-on: https://chromium-review.googlesource.com/313091
    Commit-Ready: Daniel Kurtz <djkurtz@chromium.org>
    Tested-by: Daniel Kurtz <djkurtz@chromium.org>
    Reviewed-by: Wu-cheng Li <wuchengli@chromium.org>

--- include/trace/events/v4l2.h
+++ include/trace/events/v4l2.h
@@ -58,7 +58,8 @@
 		{ V4L2_BUF_FLAG_TIMESTAMP_MASK,	     "TIMESTAMP_MASK" },      \
 		{ V4L2_BUF_FLAG_TIMESTAMP_UNKNOWN,   "TIMESTAMP_UNKNOWN" },   \
 		{ V4L2_BUF_FLAG_TIMESTAMP_MONOTONIC, "TIMESTAMP_MONOTONIC" }, \
-		{ V4L2_BUF_FLAG_TIMESTAMP_COPY,	     "TIMESTAMP_COPY" })
+		{ V4L2_BUF_FLAG_TIMESTAMP_COPY,	     "TIMESTAMP_COPY" },      \
+		{ V4L2_BUF_FLAG_LAST,                "LAST" })
 
 #define show_timecode_flags(flags)					  \
 	__print_flags(flags, "|",					  \
