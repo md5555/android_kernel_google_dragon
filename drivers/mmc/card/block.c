4abbaa0feb7cf8f5b5eca10464d70f8c346aff8d
Adrian Hunter <adrian.hunter@intel.com>
2015-05-07 13:10:24 +0300

    UPSTREAM: mmc: block: Retry errored data requests when re-tuning is needed
    Retry errored data requests when re-tuning is needed and
    add a flag to struct mmc_blk_request so that the retry
    is only done once.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    (cherry picked from commit b8360a4945c49c07568892b5958c08e01bf7d6d2
     https://git.linaro.org/people/ulf.hansson/mmc.git next)
    
    BUG=chrome-os-partner:41746
    TEST=Set mmc0/mmc1 max-frequency = <200000000> in mt8173-oak.dtsi
    
    Change-Id: I75a6d445a0335b51d9a7b40f5ef326158a371a7a
    Signed-off-by: Chaotian Jing <chaotian.jing@mediatek.com>
    Reviewed-on: https://chromium-review.googlesource.com/306524
    Commit-Ready: Wei-Ning Huang <wnhuang@chromium.org>
    Tested-by: Wei-Ning Huang <wnhuang@chromium.org>
    Reviewed-by: Wei-Ning Huang <wnhuang@chromium.org>

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -1348,6 +1348,7 @@ static int mmc_blk_err_check(struct mmc_card *card,
 						    mmc_active);
 	struct mmc_blk_request *brq = &mq_mrq->brq;
 	struct request *req = mq_mrq->req;
+	int need_retune = card->host->need_retune;
 	int ecc_err = 0, gen_err = 0;
 
 	/*
@@ -1415,6 +1416,12 @@ static int mmc_blk_err_check(struct mmc_card *card,
 	}
 
 	if (brq->data.error) {
+		if (need_retune && !brq->retune_retry_done) {
+			pr_info("%s: retrying because a re-tune was needed\n",
+				req->rq_disk->disk_name);
+			brq->retune_retry_done = 1;
+			return MMC_BLK_RETRY;
+		}
 		pr_err("%s: error %d transferring data, sector %u, nr %u, cmd response %#x, card status %#x\n",
 		       req->rq_disk->disk_name, brq->data.error,
 		       (unsigned)blk_rq_pos(req),
@@ -1983,7 +1990,7 @@ static int mmc_blk_issue_rw_rq(struct mmc_queue *mq, struct request *rqc)
 	struct mmc_blk_data *md = mq->data;
 	struct mmc_card *card = md->queue.card;
 	struct mmc_blk_request *brq = &mq->mqrq_cur->brq;
-	int ret = 1, disable_multi = 0, retry = 0, type;
+	int ret = 1, disable_multi = 0, retry = 0, type, retune_retry_done = 0;
 	enum mmc_blk_status status;
 	struct mmc_queue_req *mq_rq;
 	struct request *req = rqc;
@@ -2067,6 +2074,7 @@ static int mmc_blk_issue_rw_rq(struct mmc_queue *mq, struct request *rqc)
 				break;
 			goto cmd_abort;
 		case MMC_BLK_RETRY:
+			retune_retry_done = brq->retune_retry_done;
 			if (retry++ < 5)
 				break;
 			/* Fall through */
@@ -2129,6 +2137,7 @@ static int mmc_blk_issue_rw_rq(struct mmc_queue *mq, struct request *rqc)
 				mmc_start_req(card->host,
 						&mq_rq->mmc_active, NULL);
 			}
+			mq_rq->brq.retune_retry_done = retune_retry_done;
 		}
 	} while (ret);
 
