mortezadadgar/android_kernel_lge_bullhead

CHROMIUM: clk: tegra: declare disp1/2.emc and ispa/b.emc as SHARED_ISO_BW
Display, VI and ISP are categorized as having real-time memory
requirement so the memory bandwidth requests from those devices need
to be taken care differently against the others. Considering the
efficiency of the Tegra memory sub-system based on different dram type,
usages and frequency, expansion on the memory bandwidth request coming
from this type "SHARED_ISO_BW" of emc clients is needed to make sure
that they all can get the required bandwidth.

This patch creates a new emc shared clock mode named "SHARED_ISO_BW" and
change some of the emc clients (disp1.emc, disp2.emc, ispa.emc and
ispb.emc) to be this type so we can do some work for them to aquire the
guaranteed bandwidth.

Based on the work by: Kaz Fukoka <kfukuoka@nvidia.com>

BUG=chrome-os-partner:32032
TEST=Boot and take a look at the emc clock summary reported by the
     debugfs under /sys/kernel/debug/clk/ to see if the memory
     memory request coming from disp1/2.emc is really expanding the
     final emc rate, it should be.

Change-Id: I986cf1bd973b4116fe578bc46b1e852700bacead
Signed-off-by: Bill Huang <bilhuang@nvidia.com>
Reviewed-on: https://chromium-review.googlesource.com/222121
Reviewed-by: Dylan Reid <dgreid@chromium.org>

drivers/clk/tegra/clk-cbus-shared.c
@@ -22,6 +22,7 @@
 #include <linux/clk.h>
 #include <linux/tegra-soc.h>
 #include <linux/tegra-dvfs.h>
+#include <linux/platform_data/tegra_emc.h>
 
 #include "clk.h"
 
@@ -215,7 +216,10 @@ static int _clk_shared_bus_update(struct clk *bus)
 	unsigned long top_rate = 0;
 	unsigned long rate = cbus->min_rate;
 	unsigned long bw = 0;
+	unsigned long iso_bw = 0;
 	unsigned long ceiling = cbus->max_rate;
+	unsigned long ceiling_but_iso = cbus->max_rate;
+	unsigned long usage_flags = 0;
 
 	list_for_each_entry(c, &cbus->shared_bus_list,
 			u.shared_bus_user.node) {
@@ -227,8 +231,13 @@ static int _clk_shared_bus_update(struct clk *bus)
 		if (c->u.shared_bus_user.enabled ||
 		    (c->u.shared_bus_user.mode == SHARED_CEILING)) {
 			unsigned long request_rate = c->u.shared_bus_user.rate;
+			usage_flags |= c->iso_usages;
 
 			switch (c->u.shared_bus_user.mode) {
+			case SHARED_ISO_BW:
+				iso_bw += request_rate;
+				if (iso_bw > cbus->max_rate)
+					iso_bw = cbus->max_rate;
 			case SHARED_BW:
 				bw += request_rate;
 				if (bw > cbus->max_rate)
@@ -257,7 +266,22 @@ static int _clk_shared_bus_update(struct clk *bus)
 	 */
 	if (!top_rate)
 		rate = clk_get_rate(bus);
+
+	if (!strcmp(__clk_get_name(bus), "emc_master")) {
+		unsigned long iso_bw_min = 0;
+		struct tegra_clk_emc *emc = to_clk_emc(
+				__clk_get_hw(__clk_get_parent(bus)));
+		if (!IS_ERR(emc) && emc->emc_ops->emc_apply_efficiency) {
+			bw = emc->emc_ops->emc_apply_efficiency(
+				bw, iso_bw, cbus->max_rate, usage_flags,
+				&iso_bw_min);
+			iso_bw_min = clk_round_rate(bus, iso_bw_min);
+		}
+		ceiling_but_iso = max(ceiling_but_iso, iso_bw_min);
+	}
+
 	rate = override_rate ? : max(rate, bw);
+	ceiling = min(ceiling, ceiling_but_iso);
 	ceiling = override_rate ? cbus->max_rate : ceiling;
 
 	rate = _clk_cap_shared_bus(bus, rate, ceiling);
@@ -607,7 +631,8 @@ struct clk *tegra_clk_register_cbus(const char *name,
 
 struct clk *tegra_clk_register_shared(const char *name,
 		const char **parent, u8 num_parents, unsigned long flags,
-		enum shared_bus_users_mode mode, const char *client)
+		unsigned long usages, enum shared_bus_users_mode mode,
+		const char *client)
 {
 	struct tegra_clk_cbus_shared *shared;
 	struct clk_init_data init;
@@ -644,6 +669,7 @@ struct clk *tegra_clk_register_shared(const char *name,
 		shared->u.shared_bus_user.rate = clk_get_rate(parent_clk);
 
 	shared->flags = flags;
+	shared->iso_usages = usages;
 
 	if (num_parents > 1) {
 		struct clk *c = __clk_lookup(parent[1]);

drivers/clk/tegra/clk-tegra-shared.c
@@ -16,6 +16,7 @@
 
 #include <linux/clk.h>
 #include <linux/clk-provider.h>
+#include <linux/platform_data/tegra_emc.h>
 
 #include "clk.h"
 #include "clk-id.h"
@@ -30,75 +31,77 @@ struct tegra_shared_clk {
 	int num_parents;
 	enum shared_bus_users_mode mode;
 	int flags;
+	unsigned long usages;
 	int clk_id;
 };
 
-#define SHARED_CLK(_name, _parent, _mode, _flags, _client, _id)\
+#define SHARED_CLK(_name, _parent, _mode, _flags, _usages, _client, _id)\
 	{\
 		.name = _name,\
 		.p.parent = _parent,\
 		.num_parents = 1,\
 		.mode = _mode, \
 		.flags = _flags, \
+		.usages = _usages, \
 		.client = _client,\
 		.clk_id = _id,\
 	}
 
 static struct tegra_shared_clk shared_clks[] = {
-	SHARED_CLK("cap.c2bus", "c2bus", SHARED_CEILING, 0, NULL, tegra_clk_cap_c2bus),
-	SHARED_CLK("cap.throttle.c2bus", "c2bus", SHARED_CEILING, 0, NULL, tegra_clk_cap_throttle_c2bus),
-	SHARED_CLK("floor.c2bus", "c2bus", 0, 0, NULL, tegra_clk_floor_c2bus),
-	SHARED_CLK("override.c2bus", "c2bus", SHARED_OVERRIDE, 0, NULL, tegra_clk_override_c2bus),
-	SHARED_CLK("edp.c2bus", "c2bus", SHARED_CEILING, 0, NULL, tegra_clk_edp_c2bus),
-	SHARED_CLK("battery.c2bus", "c2bus", SHARED_CEILING, 0, NULL, tegra_clk_battery_c2bus),
-	SHARED_CLK("cap.profile.c2bus", "c2bus", SHARED_CEILING, 0, NULL, tegra_clk_cap_profile_c2bus),
-	SHARED_CLK("cap.c3bus", "c3bus", SHARED_CEILING, 0, NULL, tegra_clk_cap_c3bus),
-	SHARED_CLK("cap.throttle.c3bus", "c3bus", SHARED_CEILING, 0, NULL, tegra_clk_cap_throttle_c3bus),
-	SHARED_CLK("override.c3bus", "c3bus", SHARED_OVERRIDE, 0, NULL, tegra_clk_override_c3bus),
-	SHARED_CLK("cap.sclk", "sbus", SHARED_CEILING, 0, NULL, tegra_clk_cap_sclk),
-	SHARED_CLK("cap.throttle.sclk", "sbus", SHARED_CEILING, 0, NULL, tegra_clk_cap_throttle_sclk),
-	SHARED_CLK("floor.sclk", "sbus", 0, 0, NULL, tegra_clk_floor_sclk),
-	SHARED_CLK("override.sclk", "sbus", SHARED_OVERRIDE, 0, NULL, tegra_clk_override_sclk),
-	SHARED_CLK("avp.sclk", "sbus", 0, 0, NULL, tegra_clk_avp_sclk),
-	SHARED_CLK("bsea.sclk", "sbus", 0, 0, NULL, tegra_clk_bsea_sclk),
-	SHARED_CLK("usbd.sclk", "sbus", 0, 0, NULL, tegra_clk_usbd_sclk),
-	SHARED_CLK("usb1.sclk", "sbus", 0, 0, NULL, tegra_clk_usb1_sclk),
-	SHARED_CLK("usb2.sclk", "sbus", 0, 0, NULL, tegra_clk_usb2_sclk),
-	SHARED_CLK("usb3.sclk", "sbus", 0, 0, NULL, tegra_clk_usb3_sclk),
-	SHARED_CLK("wake.sclk", "sbus", 0, 0, NULL, tegra_clk_wake_sclk),
-	SHARED_CLK("sbc1.sclk", "sbus", 0, 0, NULL, tegra_clk_sbc1_sclk),
-	SHARED_CLK("sbc2.sclk", "sbus", 0, 0, NULL, tegra_clk_sbc2_sclk),
-	SHARED_CLK("sbc3.sclk", "sbus", 0, 0, NULL, tegra_clk_sbc3_sclk),
-	SHARED_CLK("sbc4.sclk", "sbus", 0, 0, NULL, tegra_clk_sbc4_sclk),
-	SHARED_CLK("sbc5.sclk", "sbus", 0, 0, NULL, tegra_clk_sbc5_sclk),
-	SHARED_CLK("sbc6.sclk", "sbus", 0, 0, NULL, tegra_clk_sbc6_sclk),
-	SHARED_CLK("mon.avp", "sbus", 0, 0, NULL, tegra_clk_mon_avp),
-	SHARED_CLK("avp.emc", "emc_master", 0, 0, NULL, tegra_clk_avp_emc),
-	SHARED_CLK("cpu.emc", "emc_master", 0, 0, NULL, tegra_clk_cpu_emc),
-	SHARED_CLK("disp1.emc", "emc_master", SHARED_BW, 0, NULL, tegra_clk_disp1_emc),
-	SHARED_CLK("disp2.emc", "emc_master", SHARED_BW, 0, NULL, tegra_clk_disp2_emc),
-	SHARED_CLK("hdmi.emc", "emc_master", 0, 0, NULL, tegra_clk_hdmi_emc),
-	SHARED_CLK("usbd.emc", "emc_master", 0, 0, NULL, tegra_clk_usbd_emc),
-	SHARED_CLK("usb1.emc", "emc_master", 0, 0, NULL, tegra_clk_usb1_emc),
-	SHARED_CLK("usb2.emc", "emc_master", 0, 0, NULL, tegra_clk_usb2_emc),
-	SHARED_CLK("usb3.emc", "emc_master", 0, 0, NULL, tegra_clk_usb3_emc),
-	SHARED_CLK("mon.emc", "emc_master", 0, 0, NULL, tegra_clk_mon_emc),
-	SHARED_CLK("msenc.emc", "emc_master", SHARED_BW, 0, NULL, tegra_clk_msenc_emc),
-	SHARED_CLK("tsec.emc", "emc_master", 0, 0, NULL, tegra_clk_tsec_emc),
-	SHARED_CLK("sdmmc4.emc", "emc_master", 0, 0, NULL, tegra_clk_sdmmc4_emc),
-	SHARED_CLK("camera.emc", "emc_master", SHARED_BW, 0, NULL, tegra_clk_camera_emc),
-	SHARED_CLK("iso.emc", "emc_master", SHARED_BW, 0, NULL, tegra_clk_iso_emc),
-	SHARED_CLK("cap.emc", "emc_master", SHARED_CEILING, 0, NULL, tegra_clk_cap_emc),
-	SHARED_CLK("cap.throttle.emc", "emc_master", SHARED_CEILING, 0, NULL, tegra_clk_cap_throttle_emc),
-	SHARED_CLK("floor.emc", "emc_master", 0, 0, NULL, tegra_clk_floor_emc),
-	SHARED_CLK("override.emc", "emc_master", SHARED_OVERRIDE, 0, NULL, tegra_clk_override_emc),
-	SHARED_CLK("edp.emc", "emc_master", SHARED_CEILING, 0, NULL, tegra_clk_edp_emc),
-	SHARED_CLK("battery.emc", "emc_master", SHARED_CEILING, 0, NULL, tegra_clk_battery_emc),
-	SHARED_CLK("gk20a.emc", "emc_master", 0, 0, NULL, tegra_clk_gk20a_emc),
-	SHARED_CLK("vic03.emc", "emc_master", 0, 0, NULL, tegra_clk_vic03_emc),
-	SHARED_CLK("ispa.emc", "emc_master", 0, 0, NULL, tegra_clk_ispa_emc),
-	SHARED_CLK("ispb.emc", "emc_master", 0, 0, NULL, tegra_clk_ispb_emc),
-	SHARED_CLK("xusb.emc", "emc_master", SHARED_BW, 0, NULL, tegra_clk_xusb_emc),
+	SHARED_CLK("cap.c2bus", "c2bus", SHARED_CEILING, 0, 0, NULL, tegra_clk_cap_c2bus),
+	SHARED_CLK("cap.throttle.c2bus", "c2bus", SHARED_CEILING, 0, 0, NULL, tegra_clk_cap_throttle_c2bus),
+	SHARED_CLK("floor.c2bus", "c2bus", 0, 0, 0, NULL, tegra_clk_floor_c2bus),
+	SHARED_CLK("override.c2bus", "c2bus", SHARED_OVERRIDE, 0, 0, NULL, tegra_clk_override_c2bus),
+	SHARED_CLK("edp.c2bus", "c2bus", SHARED_CEILING, 0, 0, NULL, tegra_clk_edp_c2bus),
+	SHARED_CLK("battery.c2bus", "c2bus", SHARED_CEILING, 0, 0, NULL, tegra_clk_battery_c2bus),
+	SHARED_CLK("cap.profile.c2bus", "c2bus", SHARED_CEILING, 0, 0, NULL, tegra_clk_cap_profile_c2bus),
+	SHARED_CLK("cap.c3bus", "c3bus", SHARED_CEILING, 0, 0, NULL, tegra_clk_cap_c3bus),
+	SHARED_CLK("cap.throttle.c3bus", "c3bus", SHARED_CEILING, 0, 0, NULL, tegra_clk_cap_throttle_c3bus),
+	SHARED_CLK("override.c3bus", "c3bus", SHARED_OVERRIDE, 0, 0, NULL, tegra_clk_override_c3bus),
+	SHARED_CLK("cap.sclk", "sbus", SHARED_CEILING, 0, 0, NULL, tegra_clk_cap_sclk),
+	SHARED_CLK("cap.throttle.sclk", "sbus", SHARED_CEILING, 0, 0, NULL, tegra_clk_cap_throttle_sclk),
+	SHARED_CLK("floor.sclk", "sbus", 0, 0, 0, NULL, tegra_clk_floor_sclk),
+	SHARED_CLK("override.sclk", "sbus", SHARED_OVERRIDE, 0, 0, NULL, tegra_clk_override_sclk),
+	SHARED_CLK("avp.sclk", "sbus", 0, 0, 0, NULL, tegra_clk_avp_sclk),
+	SHARED_CLK("bsea.sclk", "sbus", 0, 0, 0, NULL, tegra_clk_bsea_sclk),
+	SHARED_CLK("usbd.sclk", "sbus", 0, 0, 0, NULL, tegra_clk_usbd_sclk),
+	SHARED_CLK("usb1.sclk", "sbus", 0, 0, 0, NULL, tegra_clk_usb1_sclk),
+	SHARED_CLK("usb2.sclk", "sbus", 0, 0, 0, NULL, tegra_clk_usb2_sclk),
+	SHARED_CLK("usb3.sclk", "sbus", 0, 0, 0, NULL, tegra_clk_usb3_sclk),
+	SHARED_CLK("wake.sclk", "sbus", 0, 0, 0, NULL, tegra_clk_wake_sclk),
+	SHARED_CLK("sbc1.sclk", "sbus", 0, 0, 0, NULL, tegra_clk_sbc1_sclk),
+	SHARED_CLK("sbc2.sclk", "sbus", 0, 0, 0, NULL, tegra_clk_sbc2_sclk),
+	SHARED_CLK("sbc3.sclk", "sbus", 0, 0, 0, NULL, tegra_clk_sbc3_sclk),
+	SHARED_CLK("sbc4.sclk", "sbus", 0, 0, 0, NULL, tegra_clk_sbc4_sclk),
+	SHARED_CLK("sbc5.sclk", "sbus", 0, 0, 0, NULL, tegra_clk_sbc5_sclk),
+	SHARED_CLK("sbc6.sclk", "sbus", 0, 0, 0, NULL, tegra_clk_sbc6_sclk),
+	SHARED_CLK("mon.avp", "sbus", 0, 0, 0, NULL, tegra_clk_mon_avp),
+	SHARED_CLK("avp.emc", "emc_master", 0, 0, 0, NULL, tegra_clk_avp_emc),
+	SHARED_CLK("cpu.emc", "emc_master", 0, 0, 0, NULL, tegra_clk_cpu_emc),
+	SHARED_CLK("disp1.emc", "emc_master", SHARED_ISO_BW, 0, BIT(EMC_USER_DC1), NULL, tegra_clk_disp1_emc),
+	SHARED_CLK("disp2.emc", "emc_master", SHARED_ISO_BW, 0, BIT(EMC_USER_DC2), NULL, tegra_clk_disp2_emc),
+	SHARED_CLK("hdmi.emc", "emc_master", 0, 0, 0, NULL, tegra_clk_hdmi_emc),
+	SHARED_CLK("usbd.emc", "emc_master", 0, 0, 0, NULL, tegra_clk_usbd_emc),
+	SHARED_CLK("usb1.emc", "emc_master", 0, 0, 0, NULL, tegra_clk_usb1_emc),
+	SHARED_CLK("usb2.emc", "emc_master", 0, 0, 0, NULL, tegra_clk_usb2_emc),
+	SHARED_CLK("usb3.emc", "emc_master", 0, 0, 0, NULL, tegra_clk_usb3_emc),
+	SHARED_CLK("mon.emc", "emc_master", 0, 0, 0, NULL, tegra_clk_mon_emc),
+	SHARED_CLK("msenc.emc", "emc_master", SHARED_BW, 0, 0, NULL, tegra_clk_msenc_emc),
+	SHARED_CLK("tsec.emc", "emc_master", 0, 0, 0, NULL, tegra_clk_tsec_emc),
+	SHARED_CLK("sdmmc4.emc", "emc_master", 0, 0, 0, NULL, tegra_clk_sdmmc4_emc),
+	SHARED_CLK("camera.emc", "emc_master", SHARED_BW, 0, 0, NULL, tegra_clk_camera_emc),
+	SHARED_CLK("iso.emc", "emc_master", SHARED_BW, 0, 0, NULL, tegra_clk_iso_emc),
+	SHARED_CLK("cap.emc", "emc_master", SHARED_CEILING, 0, 0, NULL, tegra_clk_cap_emc),
+	SHARED_CLK("cap.throttle.emc", "emc_master", SHARED_CEILING, 0, 0, NULL, tegra_clk_cap_throttle_emc),
+	SHARED_CLK("floor.emc", "emc_master", 0, 0, 0, NULL, tegra_clk_floor_emc),
+	SHARED_CLK("override.emc", "emc_master", SHARED_OVERRIDE, 0, 0, NULL, tegra_clk_override_emc),
+	SHARED_CLK("edp.emc", "emc_master", SHARED_CEILING, 0, 0, NULL, tegra_clk_edp_emc),
+	SHARED_CLK("battery.emc", "emc_master", SHARED_CEILING, 0, 0, NULL, tegra_clk_battery_emc),
+	SHARED_CLK("gk20a.emc", "emc_master", 0, 0, 0, NULL, tegra_clk_gk20a_emc),
+	SHARED_CLK("vic03.emc", "emc_master", 0, 0, 0, NULL, tegra_clk_vic03_emc),
+	SHARED_CLK("ispa.emc", "emc_master", SHARED_ISO_BW, 0, BIT(EMC_USER_ISPA), NULL, tegra_clk_ispa_emc),
+	SHARED_CLK("ispb.emc", "emc_master", SHARED_ISO_BW, 0, BIT(EMC_USER_ISPB), NULL, tegra_clk_ispb_emc),
+	SHARED_CLK("xusb.emc", "emc_master", SHARED_BW, 0, 0, NULL, tegra_clk_xusb_emc),
 };
 
 void __init tegra_shared_clk_init(struct tegra_clk *tegra_clks)
@@ -121,8 +124,8 @@ void __init tegra_shared_clk_init(struct tegra_clk *tegra_clks)
 			continue;
 
 		clk = tegra_clk_register_shared(data->name, parents,
-				data->num_parents, data->flags, data->mode,
-				data->client);
+				data->num_parents, data->flags, data->usages,
+				data->mode, data->client);
 		*dt_clk = clk;
 	}
 }

md5555/android_kernel_google_dragon

(cherry picked from commit d06adc22738afb6d3d548a158cbe82c4a3ee5efc)
Conflicts:
	drivers/clk/tegra/clk-cbus-shared.c
	drivers/clk/tegra/clk-tegra-shared.c
	drivers/clk/tegra/clk-tegra114.c
Reviewed-on: https://chrome-internal-review.googlesource.com/213219
Tested-by: Danny Huang <dahuang@nvidia.com>
Commit-Queue: Dylan Reid <dgreid@chromium.org>
BUG=chrome-os-partner:40433
TEST=built and boot
Change-Id: Id2cbe7291be4f8c1e5721ffe76a8d4de87c51de6
Signed-off-by: Danny Huang <dahuang@nvidia.com>
Reviewed-on: https://chromium-review.googlesource.com/272357
Reviewed-by: Andrew Bresticker <abrestic@chromium.org>

drivers/clk/tegra/clk-cbus-shared.c
@@ -20,6 +20,7 @@
 #include <linux/err.h>
 #include <linux/clk-provider.h>
 #include <linux/clk.h>
+#include <soc/tegra/tegra_emc.h>
 
 #include "clk.h"
 
@@ -192,7 +193,10 @@ static int _clk_shared_bus_update(struct clk *bus)
 	unsigned long top_rate = 0;
 	unsigned long rate = cbus->min_rate;
 	unsigned long bw = 0;
+	unsigned long iso_bw = 0;
 	unsigned long ceiling = cbus->max_rate;
+	unsigned long ceiling_but_iso = cbus->max_rate;
+	unsigned long usage_flags = 0;
 
 	list_for_each_entry(c, &cbus->shared_bus_list,
 			u.shared_bus_user.node) {
@@ -204,8 +208,13 @@ static int _clk_shared_bus_update(struct clk *bus)
 		if (c->u.shared_bus_user.enabled ||
 		    (c->u.shared_bus_user.mode == SHARED_CEILING)) {
 			unsigned long request_rate = c->u.shared_bus_user.rate;
+			usage_flags |= c->iso_usages;
 
 			switch (c->u.shared_bus_user.mode) {
+			case SHARED_ISO_BW:
+				iso_bw += request_rate;
+				if (iso_bw > cbus->max_rate)
+					iso_bw = cbus->max_rate;
 			case SHARED_BW:
 				bw += request_rate;
 				if (bw > cbus->max_rate)
@@ -234,7 +243,22 @@ static int _clk_shared_bus_update(struct clk *bus)
 	 */
 	if (!top_rate)
 		rate = clk_get_rate(bus);
+
+	if (!strcmp(__clk_get_name(bus), "emc_master")) {
+		unsigned long iso_bw_min = 0;
+		struct tegra_clk_emc *emc = to_clk_emc(
+				__clk_get_hw(__clk_get_parent(bus)));
+		if (!IS_ERR(emc) && emc->emc_ops->emc_apply_efficiency) {
+			bw = emc->emc_ops->emc_apply_efficiency(
+				bw, iso_bw, cbus->max_rate, usage_flags,
+				&iso_bw_min);
+			iso_bw_min = clk_round_rate(bus, iso_bw_min);
+		}
+		ceiling_but_iso = max(ceiling_but_iso, iso_bw_min);
+	}
+
 	rate = override_rate ? : max(rate, bw);
+	ceiling = min(ceiling, ceiling_but_iso);
 	ceiling = override_rate ? cbus->max_rate : ceiling;
 
 	rate = _clk_cap_shared_bus(bus, rate, ceiling);
@@ -561,7 +585,8 @@ struct clk *tegra_clk_register_cbus(const char *name,
 
 struct clk *tegra_clk_register_shared(const char *name,
 		const char **parent, u8 num_parents, unsigned long flags,
-		enum shared_bus_users_mode mode, const char *client)
+		unsigned long usages, enum shared_bus_users_mode mode,
+		const char *client)
 {
 	struct tegra_clk_cbus_shared *shared;
 	struct clk_init_data init;
@@ -598,6 +623,7 @@ struct clk *tegra_clk_register_shared(const char *name,
 		shared->u.shared_bus_user.rate = clk_get_rate(parent_clk);
 
 	shared->flags = flags;
+	shared->iso_usages = usages;
 
 	if (num_parents > 1) {
 		struct clk *c = __clk_lookup(parent[1]);

drivers/clk/tegra/clk-tegra-shared.c
@@ -16,6 +16,7 @@
 
 #include <linux/clk.h>
 #include <linux/clk-provider.h>
+#include <soc/tegra/tegra_emc.h>
 
 #include "clk.h"
 #include "clk-id.h"
@@ -30,75 +31,77 @@ struct tegra_shared_clk {
 	int num_parents;
 	enum shared_bus_users_mode mode;
 	int flags;
+	unsigned long usages;
 	int clk_id;
 };
 
-#define SHARED_CLK(_name, _parent, _mode, _flags, _client, _id)\
+#define SHARED_CLK(_name, _parent, _mode, _flags, _usages, _client, _id)\
 	{\
 		.name = _name,\
 		.p.parent = _parent,\
 		.num_parents = 1,\
 		.mode = _mode, \
 		.flags = _flags, \
+		.usages = _usages, \
 		.client = _client,\
 		.clk_id = _id,\
 	}
 
 static struct tegra_shared_clk shared_clks[] = {
-	SHARED_CLK("cap.c2bus", "c2bus", SHARED_CEILING, 0, NULL, tegra_clk_cap_c2bus),
-	SHARED_CLK("cap.throttle.c2bus", "c2bus", SHARED_CEILING, 0, NULL, tegra_clk_cap_throttle_c2bus),
-	SHARED_CLK("floor.c2bus", "c2bus", 0, 0, NULL, tegra_clk_floor_c2bus),
-	SHARED_CLK("override.c2bus", "c2bus", SHARED_OVERRIDE, 0, NULL, tegra_clk_override_c2bus),
-	SHARED_CLK("edp.c2bus", "c2bus", SHARED_CEILING, 0, NULL, tegra_clk_edp_c2bus),
-	SHARED_CLK("battery.c2bus", "c2bus", SHARED_CEILING, 0, NULL, tegra_clk_battery_c2bus),
-	SHARED_CLK("cap.profile.c2bus", "c2bus", SHARED_CEILING, 0, NULL, tegra_clk_cap_profile_c2bus),
-	SHARED_CLK("cap.c3bus", "c3bus", SHARED_CEILING, 0, NULL, tegra_clk_cap_c3bus),
-	SHARED_CLK("cap.throttle.c3bus", "c3bus", SHARED_CEILING, 0, NULL, tegra_clk_cap_throttle_c3bus),
-	SHARED_CLK("override.c3bus", "c3bus", SHARED_OVERRIDE, 0, NULL, tegra_clk_override_c3bus),
-	SHARED_CLK("cap.sclk", "sbus", SHARED_CEILING, 0, NULL, tegra_clk_cap_sclk),
-	SHARED_CLK("cap.throttle.sclk", "sbus", SHARED_CEILING, 0, NULL, tegra_clk_cap_throttle_sclk),
-	SHARED_CLK("floor.sclk", "sbus", 0, 0, NULL, tegra_clk_floor_sclk),
-	SHARED_CLK("override.sclk", "sbus", SHARED_OVERRIDE, 0, NULL, tegra_clk_override_sclk),
-	SHARED_CLK("avp.sclk", "sbus", 0, 0, NULL, tegra_clk_avp_sclk),
-	SHARED_CLK("bsea.sclk", "sbus", 0, 0, NULL, tegra_clk_bsea_sclk),
-	SHARED_CLK("usbd.sclk", "sbus", 0, 0, NULL, tegra_clk_usbd_sclk),
-	SHARED_CLK("usb1.sclk", "sbus", 0, 0, NULL, tegra_clk_usb1_sclk),
-	SHARED_CLK("usb2.sclk", "sbus", 0, 0, NULL, tegra_clk_usb2_sclk),
-	SHARED_CLK("usb3.sclk", "sbus", 0, 0, NULL, tegra_clk_usb3_sclk),
-	SHARED_CLK("wake.sclk", "sbus", 0, 0, NULL, tegra_clk_wake_sclk),
-	SHARED_CLK("sbc1.sclk", "sbus", 0, 0, NULL, tegra_clk_sbc1_sclk),
-	SHARED_CLK("sbc2.sclk", "sbus", 0, 0, NULL, tegra_clk_sbc2_sclk),
-	SHARED_CLK("sbc3.sclk", "sbus", 0, 0, NULL, tegra_clk_sbc3_sclk),
-	SHARED_CLK("sbc4.sclk", "sbus", 0, 0, NULL, tegra_clk_sbc4_sclk),
-	SHARED_CLK("sbc5.sclk", "sbus", 0, 0, NULL, tegra_clk_sbc5_sclk),
-	SHARED_CLK("sbc6.sclk", "sbus", 0, 0, NULL, tegra_clk_sbc6_sclk),
-	SHARED_CLK("mon.avp", "sbus", 0, 0, NULL, tegra_clk_mon_avp),
-	SHARED_CLK("avp.emc", "emc_master", 0, 0, NULL, tegra_clk_avp_emc),
-	SHARED_CLK("cpu.emc", "emc_master", 0, 0, NULL, tegra_clk_cpu_emc),
-	SHARED_CLK("disp1.emc", "emc_master", SHARED_BW, 0, NULL, tegra_clk_disp1_emc),
-	SHARED_CLK("disp2.emc", "emc_master", SHARED_BW, 0, NULL, tegra_clk_disp2_emc),
-	SHARED_CLK("hdmi.emc", "emc_master", 0, 0, NULL, tegra_clk_hdmi_emc),
-	SHARED_CLK("usbd.emc", "emc_master", 0, 0, NULL, tegra_clk_usbd_emc),
-	SHARED_CLK("usb1.emc", "emc_master", 0, 0, NULL, tegra_clk_usb1_emc),
-	SHARED_CLK("usb2.emc", "emc_master", 0, 0, NULL, tegra_clk_usb2_emc),
-	SHARED_CLK("usb3.emc", "emc_master", 0, 0, NULL, tegra_clk_usb3_emc),
-	SHARED_CLK("mon.emc", "emc_master", 0, 0, NULL, tegra_clk_mon_emc),
-	SHARED_CLK("msenc.emc", "emc_master", SHARED_BW, 0, NULL, tegra_clk_msenc_emc),
-	SHARED_CLK("tsec.emc", "emc_master", 0, 0, NULL, tegra_clk_tsec_emc),
-	SHARED_CLK("sdmmc4.emc", "emc_master", 0, 0, NULL, tegra_clk_sdmmc4_emc),
-	SHARED_CLK("camera.emc", "emc_master", SHARED_BW, 0, NULL, tegra_clk_camera_emc),
-	SHARED_CLK("iso.emc", "emc_master", SHARED_BW, 0, NULL, tegra_clk_iso_emc),
-	SHARED_CLK("cap.emc", "emc_master", SHARED_CEILING, 0, NULL, tegra_clk_cap_emc),
-	SHARED_CLK("cap.throttle.emc", "emc_master", SHARED_CEILING, 0, NULL, tegra_clk_cap_throttle_emc),
-	SHARED_CLK("floor.emc", "emc_master", 0, 0, NULL, tegra_clk_floor_emc),
-	SHARED_CLK("override.emc", "emc_master", SHARED_OVERRIDE, 0, NULL, tegra_clk_override_emc),
-	SHARED_CLK("edp.emc", "emc_master", SHARED_CEILING, 0, NULL, tegra_clk_edp_emc),
-	SHARED_CLK("battery.emc", "emc_master", SHARED_CEILING, 0, NULL, tegra_clk_battery_emc),
-	SHARED_CLK("gk20a.emc", "emc_master", 0, 0, NULL, tegra_clk_gk20a_emc),
-	SHARED_CLK("vic03.emc", "emc_master", 0, 0, NULL, tegra_clk_vic03_emc),
-	SHARED_CLK("ispa.emc", "emc_master", 0, 0, NULL, tegra_clk_ispa_emc),
-	SHARED_CLK("ispb.emc", "emc_master", 0, 0, NULL, tegra_clk_ispb_emc),
-	SHARED_CLK("xusb.emc", "emc_master", 0, 0, NULL, tegra_clk_xusb_emc),
+	SHARED_CLK("cap.c2bus", "c2bus", SHARED_CEILING, 0, 0, NULL, tegra_clk_cap_c2bus),
+	SHARED_CLK("cap.throttle.c2bus", "c2bus", SHARED_CEILING, 0, 0, NULL, tegra_clk_cap_throttle_c2bus),
+	SHARED_CLK("floor.c2bus", "c2bus", 0, 0, 0, NULL, tegra_clk_floor_c2bus),
+	SHARED_CLK("override.c2bus", "c2bus", SHARED_OVERRIDE, 0, 0, NULL, tegra_clk_override_c2bus),
+	SHARED_CLK("edp.c2bus", "c2bus", SHARED_CEILING, 0, 0, NULL, tegra_clk_edp_c2bus),
+	SHARED_CLK("battery.c2bus", "c2bus", SHARED_CEILING, 0, 0, NULL, tegra_clk_battery_c2bus),
+	SHARED_CLK("cap.profile.c2bus", "c2bus", SHARED_CEILING, 0, 0, NULL, tegra_clk_cap_profile_c2bus),
+	SHARED_CLK("cap.c3bus", "c3bus", SHARED_CEILING, 0, 0, NULL, tegra_clk_cap_c3bus),
+	SHARED_CLK("cap.throttle.c3bus", "c3bus", SHARED_CEILING, 0, 0, NULL, tegra_clk_cap_throttle_c3bus),
+	SHARED_CLK("override.c3bus", "c3bus", SHARED_OVERRIDE, 0, 0, NULL, tegra_clk_override_c3bus),
+	SHARED_CLK("cap.sclk", "sbus", SHARED_CEILING, 0, 0, NULL, tegra_clk_cap_sclk),
+	SHARED_CLK("cap.throttle.sclk", "sbus", SHARED_CEILING, 0, 0, NULL, tegra_clk_cap_throttle_sclk),
+	SHARED_CLK("floor.sclk", "sbus", 0, 0, 0, NULL, tegra_clk_floor_sclk),
+	SHARED_CLK("override.sclk", "sbus", SHARED_OVERRIDE, 0, 0, NULL, tegra_clk_override_sclk),
+	SHARED_CLK("avp.sclk", "sbus", 0, 0, 0, NULL, tegra_clk_avp_sclk),
+	SHARED_CLK("bsea.sclk", "sbus", 0, 0, 0, NULL, tegra_clk_bsea_sclk),
+	SHARED_CLK("usbd.sclk", "sbus", 0, 0, 0, NULL, tegra_clk_usbd_sclk),
+	SHARED_CLK("usb1.sclk", "sbus", 0, 0, 0, NULL, tegra_clk_usb1_sclk),
+	SHARED_CLK("usb2.sclk", "sbus", 0, 0, 0, NULL, tegra_clk_usb2_sclk),
+	SHARED_CLK("usb3.sclk", "sbus", 0, 0, 0, NULL, tegra_clk_usb3_sclk),
+	SHARED_CLK("wake.sclk", "sbus", 0, 0, 0, NULL, tegra_clk_wake_sclk),
+	SHARED_CLK("sbc1.sclk", "sbus", 0, 0, 0, NULL, tegra_clk_sbc1_sclk),
+	SHARED_CLK("sbc2.sclk", "sbus", 0, 0, 0, NULL, tegra_clk_sbc2_sclk),
+	SHARED_CLK("sbc3.sclk", "sbus", 0, 0, 0, NULL, tegra_clk_sbc3_sclk),
+	SHARED_CLK("sbc4.sclk", "sbus", 0, 0, 0, NULL, tegra_clk_sbc4_sclk),
+	SHARED_CLK("sbc5.sclk", "sbus", 0, 0, 0, NULL, tegra_clk_sbc5_sclk),
+	SHARED_CLK("sbc6.sclk", "sbus", 0, 0, 0, NULL, tegra_clk_sbc6_sclk),
+	SHARED_CLK("mon.avp", "sbus", 0, 0, 0, NULL, tegra_clk_mon_avp),
+	SHARED_CLK("avp.emc", "emc_master", 0, 0, 0, NULL, tegra_clk_avp_emc),
+	SHARED_CLK("cpu.emc", "emc_master", 0, 0, 0, NULL, tegra_clk_cpu_emc),
+	SHARED_CLK("disp1.emc", "emc_master", SHARED_ISO_BW, 0, BIT(EMC_USER_DC1), NULL, tegra_clk_disp1_emc),
+	SHARED_CLK("disp2.emc", "emc_master", SHARED_ISO_BW, 0, BIT(EMC_USER_DC2), NULL, tegra_clk_disp2_emc),
+	SHARED_CLK("hdmi.emc", "emc_master", 0, 0, 0, NULL, tegra_clk_hdmi_emc),
+	SHARED_CLK("usbd.emc", "emc_master", 0, 0, 0, NULL, tegra_clk_usbd_emc),
+	SHARED_CLK("usb1.emc", "emc_master", 0, 0, 0, NULL, tegra_clk_usb1_emc),
+	SHARED_CLK("usb2.emc", "emc_master", 0, 0, 0, NULL, tegra_clk_usb2_emc),
+	SHARED_CLK("usb3.emc", "emc_master", 0, 0, 0, NULL, tegra_clk_usb3_emc),
+	SHARED_CLK("mon.emc", "emc_master", 0, 0, 0, NULL, tegra_clk_mon_emc),
+	SHARED_CLK("msenc.emc", "emc_master", SHARED_BW, 0, 0, NULL, tegra_clk_msenc_emc),
+	SHARED_CLK("tsec.emc", "emc_master", 0, 0, 0, NULL, tegra_clk_tsec_emc),
+	SHARED_CLK("sdmmc4.emc", "emc_master", 0, 0, 0, NULL, tegra_clk_sdmmc4_emc),
+	SHARED_CLK("camera.emc", "emc_master", SHARED_BW, 0, 0, NULL, tegra_clk_camera_emc),
+	SHARED_CLK("iso.emc", "emc_master", SHARED_BW, 0, 0, NULL, tegra_clk_iso_emc),
+	SHARED_CLK("cap.emc", "emc_master", SHARED_CEILING, 0, 0, NULL, tegra_clk_cap_emc),
+	SHARED_CLK("cap.throttle.emc", "emc_master", SHARED_CEILING, 0, 0, NULL, tegra_clk_cap_throttle_emc),
+	SHARED_CLK("floor.emc", "emc_master", 0, 0, 0, NULL, tegra_clk_floor_emc),
+	SHARED_CLK("override.emc", "emc_master", SHARED_OVERRIDE, 0, 0, NULL, tegra_clk_override_emc),
+	SHARED_CLK("edp.emc", "emc_master", SHARED_CEILING, 0, 0, NULL, tegra_clk_edp_emc),
+	SHARED_CLK("battery.emc", "emc_master", SHARED_CEILING, 0, 0, NULL, tegra_clk_battery_emc),
+	SHARED_CLK("gk20a.emc", "emc_master", 0, 0, 0, NULL, tegra_clk_gk20a_emc),
+	SHARED_CLK("vic03.emc", "emc_master", 0, 0, 0, NULL, tegra_clk_vic03_emc),
+	SHARED_CLK("ispa.emc", "emc_master", SHARED_ISO_BW, 0, BIT(EMC_USER_ISPA), NULL, tegra_clk_ispa_emc),
+	SHARED_CLK("ispb.emc", "emc_master", SHARED_ISO_BW, 0, BIT(EMC_USER_ISPB), NULL, tegra_clk_ispb_emc),
+	SHARED_CLK("xusb.emc", "emc_master", SHARED_BW, 0, 0, NULL, tegra_clk_xusb_emc),
 };
 
 void __init tegra_shared_clk_init(struct tegra_clk *tegra_clks)
@@ -121,8 +124,8 @@ void __init tegra_shared_clk_init(struct tegra_clk *tegra_clks)
 			continue;
 
 		clk = tegra_clk_register_shared(data->name, parents,
-				data->num_parents, data->flags, data->mode,
-				data->client);
+				data->num_parents, data->flags, data->usages,
+				data->mode, data->client);
 		*dt_clk = clk;
 	}
 }