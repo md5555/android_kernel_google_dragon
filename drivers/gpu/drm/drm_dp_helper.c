f5ece75bdee466b050ed14499f44bd5cc4e61b92
Thierry Reding <treding@nvidia.com>
2015-07-07 20:53:36 +0200

    UPSTREAM: drm/dp: Enhanced framing capability is DP 1.1+
    The enhanced framing capability was added in DisplayPort 1.1, so any
    code dealing with it needs to be protected accordingly.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>
    [from https://github.com/thierryreding/linux.git]
    (cherry picked from commit 470131671f228b373ad81e954e49d9f580e67b41)
    Signed-off-by: Sean Paul <seanpaul@chromium.org>
    
    Change-Id: I188c5c0cc545a584aad41479d75f4d12f860f2b4
    Reviewed-on: https://chromium-review.googlesource.com/338540
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Tested-by: Sean Paul <seanpaul@chromium.org>
    Commit-Queue: Sean Paul <seanpaul@chromium.org>

--- drivers/gpu/drm/drm_dp_helper.c
+++ drivers/gpu/drm/drm_dp_helper.c
@@ -499,8 +499,9 @@ int drm_dp_link_probe(struct drm_dp_aux *aux, struct drm_dp_link *link)
 	link->rate = drm_dp_bw_code_to_link_rate(values[1]);
 	link->num_lanes = values[2] & DP_MAX_LANE_COUNT_MASK;
 
-	if (values[2] & DP_ENHANCED_FRAME_CAP)
-		link->capabilities |= DP_LINK_CAP_ENHANCED_FRAMING;
+	if (link->revision >= 0x11)
+		if (values[2] & DP_ENHANCED_FRAME_CAP)
+			link->capabilities |= DP_LINK_CAP_ENHANCED_FRAMING;
 
 	if (link->revision >= 0x12)
 		if (values[2] & DP_TPS3_SUPPORTED)
