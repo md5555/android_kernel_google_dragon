9c15e24333e2ab682fd6484679930bb9f1cc4512
Krzysztof Kozlowski <k.kozlowski@samsung.com>
2015-07-10 14:44:00 +0900

    UPSTREAM: regulator: drivers: Drop owner assignment from i2c_driver
    i2c_driver does not need to set an owner because i2c_register_driver()
    will set it.
    
    BUG=chrome-os-partner:45774
    TEST=build and boot SMAUG EVT2
    
    Change-Id: Ie8224728cc7bbe1d7ade00f39feac93efc969fea
    Signed-off-by: Krzysztof Kozlowski <k.kozlowski@samsung.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Reviewed-on: https://chromium-review.googlesource.com/297931
    Commit-Ready: Hao (Alex) Lei <alei@nvidia.com>
    Tested-by: Hao (Alex) Lei <alei@nvidia.com>
    Reviewed-by: Rhyland Klein <rklein@nvidia.com>
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>
    Reviewed-by: Hao (Alex) Lei <alei@nvidia.com>
    Reviewed-by: Laxman Dewangan <dewangan.laxman@gmail.com>

--- drivers/regulator/max8973-regulator.c
+++ drivers/regulator/max8973-regulator.c
@@ -864,7 +864,6 @@ static struct i2c_driver max8973_i2c_driver = {
 	.driver = {
 		.name = "max8973",
 		.of_match_table = of_max8973_match_tbl,
-		.owner = THIS_MODULE,
 	},
 	.probe = max8973_probe,
 	.remove = max8973_remove,
