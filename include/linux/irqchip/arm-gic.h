8804012c1f39aef558be01bd69b4f9839e87cf08
Daniel Kurtz <djkurtz@chromium.org>
2015-07-13 23:48:06 +0800

    Revert "CHROMIUM: arm: t210: SoC audio drivers for NVidia T210"
    This reverts commit c3f486103c39b35744ee9527ef28c09e265815d1.
    
    This patch modifies drivers/irqchip/irq-gic.c, essentially undoing the
    following UPSTREAM patch, which is required to boot oak:
     commit 3d52ac780ce9e050007d1cf64d8a211cabcc84d8
     UPSTREAM: irqchip: gic: Support hierarchy irq domain.
    
    BUG=chrome-os-partner:42565
    TEST=build and boot oak-rev2
    TEST=emerge-smaug chromeos-kernel-3_18
    
    Change-Id: Ia3362a44b2007f6b6290a0f00fb897dc167cc174
    Reviewed-on: https://chromium-review.googlesource.com/284964
    Reviewed-by: Anatol Pomazau <anatol@google.com>
    Commit-Queue: Anatol Pomazau <anatol@google.com>
    Tested-by: Anatol Pomazau <anatol@google.com>

--- include/linux/irqchip/arm-gic.h
+++ include/linux/irqchip/arm-gic.h
@@ -2,7 +2,6 @@
  *  include/linux/irqchip/arm-gic.h
  *
  *  Copyright (C) 2002 ARM Limited, All Rights Reserved.
- *  Copyright (C) 2014, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -27,8 +26,6 @@
 #define GICC_IAR_INT_ID_MASK		0x3ff
 #define GICC_INT_SPURIOUS		1023
 #define GICC_DIS_BYPASS_MASK		0x1e0
-
-#define GICC_IAR_INT_ID_MASK            0x3ff
 
 #define GIC_DIST_CTRL			0x000
 #define GIC_DIST_CTR			0x004
@@ -92,13 +89,9 @@
 #define GICH_MISR_EOI			(1 << 0)
 #define GICH_MISR_U			(1 << 1)
 
-enum domain_gic_pm_event {
-	MOD_DOMAIN_POWER_ON,
-	MOD_DOMAIN_POWER_OFF
-};
+#ifndef __ASSEMBLY__
 
 #include <linux/irqdomain.h>
-#ifndef __ASSEMBLY__
 
 struct device_node;
 
