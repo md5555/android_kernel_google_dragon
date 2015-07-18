19c0f1cdf2000f858efef9832ab614eb0d6f1a1c
Vince Hsu <vince.h@nvidia.com>
2015-07-18 22:28:19 +0800

    drm/nouveau/fb: really use GM20B FB device
    The commit a1976b117aee "drm/nouveau/fb: initialize active LTC number"
    was rebased carelessly and did not refer to GM20B FB device handle. This
    patch fixes that.
    
    Change-Id: I4e78906eaa8366c952cba0e367770c3c9843c3e8
    Signed-off-by: Vince Hsu <vince.h@nvidia.com>
    Reviewed-on: https://chromium-review.googlesource.com/286296
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    Tested-by: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/nouveau/nvkm/engine/device/gm100.c
+++ drivers/gpu/drm/nouveau/nvkm/engine/device/gm100.c
@@ -188,7 +188,7 @@ gm100_identify(struct nvkm_device *device)
 		device->oclass[NVDEV_SUBDEV_BUS    ] =  gf100_bus_oclass;
 		device->oclass[NVDEV_SUBDEV_FUSE   ] = &gm107_fuse_oclass;
 		device->oclass[NVDEV_SUBDEV_TIMER  ] = &gk20a_timer_oclass;
-		device->oclass[NVDEV_SUBDEV_FB     ] =  gk20a_fb_oclass;
+		device->oclass[NVDEV_SUBDEV_FB     ] =  gm20b_fb_oclass;
 		device->oclass[NVDEV_SUBDEV_LTC    ] =  gm107_ltc_oclass;
 		device->oclass[NVDEV_SUBDEV_IBUS   ] = &gk20a_ibus_oclass;
 		device->oclass[NVDEV_SUBDEV_INSTMEM] = gk20a_instmem_oclass;
