3ca51e999ac5dfa33b49c589797d0716c6818182
Dominik Behr <dbehr@chromium.org>
2015-08-11 12:02:53 -0700

    CHROMIUM: tegra/dc fix array overrun for UV stride
    Stride for plane 1 and 2 has to be the same and is stored in 2nd
    entry in the stride array. This changes makes sure the loop doesn't
    go out of bounds and removes erroneous copy of stride from window 1 to 2.
    
    BUG=none
    TEST=compile chromeos-kernel-3_18 for ryu
    
    Change-Id: I124da9462995d2b65ebbac613a77415274050eb8
    Signed-off-by: Dominik Behr <dbehr@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/292900
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/tegra/dc.c
+++ drivers/gpu/drm/tegra/dc.c
@@ -734,7 +734,9 @@ static void tegra_plane_atomic_update(struct drm_plane *plane,
 		struct tegra_bo *bo = tegra_fb_get_plane(fb, i);
 
 		window.base[i] = bo->paddr + fb->offsets[i];
-		window.stride[i] = fb->pitches[i];
+		if (i < 2)
+			window.stride[i] = fb->pitches[i];
+		
 	}
 
 	/* for semiplanar modes both U and V planes should point to the same
@@ -743,7 +745,6 @@ static void tegra_plane_atomic_update(struct drm_plane *plane,
 	if (drm_format_num_planes(fb->pixel_format) == 2 &&
 	    tegra_dc_format_is_yuv(window.format, NULL)) {
 	    window.base[2] = window.base[1];
-	    window.stride[2] = window.stride[1];
 	}
 
 	tegra_dc_setup_window(dc, p->index, &window);
