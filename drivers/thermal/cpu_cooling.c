ccb473b1bcf3d3d12afa595df863924566321308
Vaishali Thakkar <vthakkar1994@gmail.com>
2015-08-19 11:52:19 +0530

    UPSTREAM: thermal: cpu_cooling: Remove usage of devm functions
    In the function cpufreq_get_requested_power, the memory allocated
    for load_cpu is live within the function only. And after the
    allocation it is immediately freed with devm_kfree. There is no
    need to allocate memory for load_cpu with devm function so replace
    devm_kcalloc with kcalloc and devm_kfree with kfree.
    
    Signed-off-by: Vaishali Thakkar <vthakkar1994@gmail.com>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Acked-by: Javi Merino <javi.merino@arm.com>
    Signed-off-by: Zhang Rui <rui.zhang@intel.com>
    (cherry picked from commit a71544cd9373b879bbb40f55ce4d01556a4bc29e
     git://git.kernel.org/pub/scm/linux/kernel/git/rzhang/linux next)
    Signed-off-by: Daniel Kurtz <djkurtz@chromium.org>
    
    BUG=chrome-os-partner:43542
    TEST=1. (on oak) Check if battery information is provided:
      localhost ~ # power_supply_info
      Device: Line Power
        path:                    /sys/class/power_supply/CROS_USB_PD_CHARGER0
        online:                  yes
        type:                    USB
        enum type:               AC
        voltage (V):             19.859
        current (A):             0
        max voltage (V):         20
        max current (A):         3
        active source:           CROS_USB_PD_CHARGER0
        available sources:       CROS_USB_PD_CHARGER0 [0/700]
        supports dual-role:      yes
      Device: Battery
        path:                    /sys/class/power_supply/sbs-0-000b
        vendor:                  Simplo
        model name:              0C9
        serial number:           0132
        state:                   Charging
        voltage (V):             7.707
        energy (Wh):             3.4352
        energy rate (W):         14.7897
        current (A):             1.919
        charge (Ah):             0.452
        full charge (Ah):        4.817
        full charge design (Ah): 4.87
        percentage:              9.38343
        display percentage:      9.67364
        technology:              Li-ion
    TEST=2. (on oak) check if governors are power_allocator.
    
     # cat /sys/class/thermal/thermal_zone*/type
     cpu_thermal
     sbs-0-000b
     # cat /sys/class/thermal/thermal_zone*/policy
     power_allocator
     power_allocator
    
    Change-Id: Ic98517a4599096324eaa09f0cba6242605663021
    Reviewed-on: https://chromium-review.googlesource.com/297670
    Commit-Ready: Daniel Kurtz <djkurtz@chromium.org>
    Tested-by: Daniel Kurtz <djkurtz@chromium.org>
    Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>

--- drivers/thermal/cpu_cooling.c
+++ drivers/thermal/cpu_cooling.c
@@ -585,8 +585,7 @@ static int cpufreq_get_requested_power(struct thermal_cooling_device *cdev,
 	if (trace_thermal_power_cpu_get_power_enabled()) {
 		u32 ncpus = cpumask_weight(&cpufreq_device->allowed_cpus);
 
-		load_cpu = devm_kcalloc(&cdev->device, ncpus, sizeof(*load_cpu),
-					GFP_KERNEL);
+		load_cpu = kcalloc(ncpus, sizeof(*load_cpu), GFP_KERNEL);
 	}
 
 	for_each_cpu(cpu, &cpufreq_device->allowed_cpus) {
@@ -609,8 +608,7 @@ static int cpufreq_get_requested_power(struct thermal_cooling_device *cdev,
 	dynamic_power = get_dynamic_power(cpufreq_device, freq);
 	ret = get_static_power(cpufreq_device, tz, freq, &static_power);
 	if (ret) {
-		if (load_cpu)
-			devm_kfree(&cdev->device, load_cpu);
+		kfree(load_cpu);
 		return ret;
 	}
 
@@ -619,7 +617,7 @@ static int cpufreq_get_requested_power(struct thermal_cooling_device *cdev,
 			&cpufreq_device->allowed_cpus,
 			freq, load_cpu, i, dynamic_power, static_power);
 
-		devm_kfree(&cdev->device, load_cpu);
+		kfree(load_cpu);
 	}
 
 	*power = static_power + dynamic_power;
