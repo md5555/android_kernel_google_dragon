f4f605c03edf6bcd30592c4f825e4088e65d06da
Mark Kuo <mkuo@nvidia.com>
2015-08-20 12:41:30 +0800

    CHROMIUM: usb: gadget: f_fs: Choose the right descriptors
    Use config_ep_by_speed() to choose the right descriptors for an endpoint
    according to the gadget's speed.
    
    BUG=chrome-os-partner:43682
    TEST=Smaug enumerates as a SuperSpeed device.
    
    Change-Id: I81944c6f74e2ab1097981e7b34babe6bd1da9006
    Signed-off-by: Henry Lin <henryl@nvidia.com>
    Signed-off-by: Mark Kuo <mkuo@nvidia.com>
    Signed-off-by: Andrew Bresticker <abrestic@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/294718

--- drivers/usb/gadget/function/f_fs.c
+++ drivers/usb/gadget/function/f_fs.c
@@ -1648,33 +1648,17 @@ static int ffs_func_eps_enable(struct ffs_function *func)
 
 	spin_lock_irqsave(&func->ffs->eps_lock, flags);
 	do {
-		struct usb_endpoint_descriptor *ds;
-		int desc_idx;
-
-		if (ffs->gadget->speed == USB_SPEED_SUPER)
-			desc_idx = 2;
-		else if (ffs->gadget->speed == USB_SPEED_HIGH)
-			desc_idx = 1;
-		else
-			desc_idx = 0;
-
-		/* fall-back to lower speed if desc missing for current speed */
-		do {
-			ds = ep->descs[desc_idx];
-		} while (!ds && --desc_idx >= 0);
-
-		if (!ds) {
-			ret = -EINVAL;
-			break;
-		}
-
 		ep->ep->driver_data = ep;
-		ep->ep->desc = ds;
+
+		ret = config_ep_by_speed(ffs->gadget, &func->function, ep->ep);
+		if (ret)
+			break;
+
 		ret = usb_ep_enable(ep->ep);
 		if (likely(!ret)) {
 			epfile->ep = ep;
-			epfile->in = usb_endpoint_dir_in(ds);
-			epfile->isoc = usb_endpoint_xfer_isoc(ds);
+			epfile->in = usb_endpoint_dir_in(ep->ep->desc);
+			epfile->isoc = usb_endpoint_xfer_isoc(ep->ep->desc);
 		} else {
 			break;
 		}
