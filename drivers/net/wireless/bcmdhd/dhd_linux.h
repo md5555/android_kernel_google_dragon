354c2398abff39e272483fc1f8e8716b7631c7b3
Dmitry Torokhov <dtor@chromium.org>
2015-08-27 09:29:46 -0700

    CHROMIUM: net: wireless: bcmdhd: remove duplicate platform data definition
    Having platform data in two different places, depending on Kconfig
    option, is fragile. Given that option only controls where the structure
    and flags are defined, it does not make much sense.
    
    BUG=b:25423561
    TEST=Use Wifi on Smaug
    
    Change-Id: Ic14afc3b3f8e248b45eaf60a712d95147c396668
    Signed-off-by: Dmitry Torokhov <dtor@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/295871
    Reviewed-by: Andrew Bresticker <abrestic@chromium.org>

--- drivers/net/wireless/bcmdhd/dhd_linux.h
+++ drivers/net/wireless/bcmdhd/dhd_linux.h
@@ -47,32 +47,7 @@
 #if defined(CONFIG_HAS_EARLYSUSPEND) && defined(DHD_USE_EARLYSUSPEND)
 #include <linux/earlysuspend.h>
 #endif /* defined(CONFIG_HAS_EARLYSUSPEND) && defined(DHD_USE_EARLYSUSPEND) */
-#if defined(CONFIG_WIFI_CONTROL_FUNC)
 #include <linux/wlan_plat.h>
-#endif
-
-#if !defined(CONFIG_WIFI_CONTROL_FUNC)
-#define WLAN_PLAT_NODFS_FLAG	0x01
-#define WLAN_PLAT_AP_FLAG	0x02
-
-struct wifi_platform_data {
-	int (*set_power)(int val);
-	int (*set_reset)(int val);
-	int (*set_carddetect)(int val);
-	void *(*mem_prealloc)(int section, unsigned long size);
-	int (*get_mac_addr)(unsigned char *buf);
-	int (*get_wake_irq)(void);
-	void *(*get_country_code)(char *ccode, u32 flags);
-#ifdef CONFIG_PARTIALRESUME
-#define WIFI_PR_INIT			0
-#define WIFI_PR_NOTIFY_RESUME		1
-#define WIFI_PR_VOTE_FOR_RESUME		2
-#define WIFI_PR_VOTE_FOR_SUSPEND	3
-#define WIFI_PR_WAIT_FOR_READY		4
-	bool (*partial_resume)(int action);
-#endif
-};
-#endif /* CONFIG_WIFI_CONTROL_FUNC */
 
 #define DHD_REGISTRATION_TIMEOUT  12000  /* msec : allowed time to finished dhd registration */
 
