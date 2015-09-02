129fe05e4771582174757875eaad89af16302b62
Keith Tzeng <keith.tzeng@quantatw.com>
2015-09-02 13:31:42 +0800

    CHROMIUM: ASoC: rt5645: set platform data base on DMI
    set platform specific data for ultima
    Use strago setting.
    
    BUG=chrome-os-partner:44438
    TEST=See Jack detect have proper function when using ultima coreboot
    Signed-off-by: Keith Tzeng <Keith.Tzeng@quantatw.com>
    
    Change-Id: I6fbc027cb103912043ab7cc233d3d9485e57159f
    Reviewed-on: https://chromium-review.googlesource.com/296841
    Commit-Ready: Keith Tzeng <keith.tzeng@quantatw.com>
    Tested-by: Keith Tzeng <keith.tzeng@quantatw.com>
    Reviewed-by: Bernie Thompson <bhthompson@chromium.org>

--- sound/soc/codecs/rt5645.c
+++ sound/soc/codecs/rt5645.c
@@ -3206,6 +3206,13 @@ static struct dmi_system_id dmi_platform_intel_braswell[] = {
 			DMI_MATCH(DMI_PRODUCT_NAME, "Celes"),
 		},
 	},
+	{
+		.ident = "Google Ultima",
+		.callback = strago_quirk_cb,
+		.matches = {
+			DMI_MATCH(DMI_PRODUCT_NAME, "Ultima"),
+		},
+	},
 	{ }
 };
 
