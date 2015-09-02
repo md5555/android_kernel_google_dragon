d60e50ac25ba9d97782ec4f050c2fd525b62df35
Maneet Singh <mmaneetsingh@nvidia.com>
2015-09-01 18:47:42 -0700

    CHROMIUM: android: ion: tegra: Add support for T210 ion heaps
    Add support to use tegra_ion driver to define necessary ion heaps
    instead of ion_dummy_driver. Currently, heaps for T210 are defined.
    
    BUG=chrome-os-partner:39549
    TEST=Play secure content using ExoPlayer
    
    Change-Id: Ib5761954ce1d61bc5808a28333bf6b49c052b558
    Signed-off-by: Maneet Singh <mmaneetsingh@nvidia.com>
    Reviewed-on: https://chromium-review.googlesource.com/296775
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>
    Reviewed-by: Puneet Kumar <puneetster@chromium.org>

--- drivers/staging/android/ion/tegra/tegra_ion.c
+++ drivers/staging/android/ion/tegra/tegra_ion.c
@@ -15,6 +15,7 @@
  */
 
 #include <linux/err.h>
+#include <linux/of_device.h>
 #include <linux/platform_device.h>
 #include <linux/slab.h>
 #include "../ion.h"
@@ -24,25 +25,89 @@ static struct ion_device *idev;
 static int num_heaps;
 static struct ion_heap **heaps;
 
+static struct ion_platform_heap tegra210_heaps[] = {
+	{
+		.id	= ION_HEAP_TYPE_SYSTEM,
+		.type	= ION_HEAP_TYPE_SYSTEM,
+		.name	= "system",
+	},
+	{
+		.id	= ION_HEAP_TYPE_SYSTEM_CONTIG,
+		.type	= ION_HEAP_TYPE_SYSTEM_CONTIG,
+		.name	= "system contig",
+	},
+	{
+		.id	= ION_HEAP_TYPE_CARVEOUT,
+		.type	= ION_HEAP_TYPE_CARVEOUT,
+		.name	= "vpr",
+		.flags	= ION_HEAP_FLAG_DEVICE_MEM,
+	},
+};
+
+static struct ion_platform_data tegra210_ion_pdata = {
+	.nr = ARRAY_SIZE(tegra210_heaps),
+	.heaps = tegra210_heaps,
+};
+
+static const struct of_device_id ion_of_ids[] = {
+	{
+		.compatible = "nvidia,tegra210-ion-heaps",
+		.data = &tegra210_ion_pdata
+	},
+	{},
+};
+
+static ion_phys_addr_t vpr_start;
+static size_t vpr_size;
+
+static int __init tegra_vpr_arg(char *p)
+{
+	vpr_size = memparse(p, &p);
+	if (*p == '@')
+		vpr_start = memparse(p + 1, NULL);
+
+	pr_info("Found vpr, start=0x%lx size=0x%zx\n", vpr_start, vpr_size);
+	return 0;
+}
+early_param("vpr", tegra_vpr_arg);
+
 static int tegra_ion_probe(struct platform_device *pdev)
 {
-	struct ion_platform_data *pdata = pdev->dev.platform_data;
+	const struct of_device_id *match;
+	struct ion_platform_data *pdata;
 	int err;
 	int i;
 
-	num_heaps = pdata->nr;
+	match = of_match_device(ion_of_ids, &pdev->dev);
+	if (!match) {
+		dev_err(&pdev->dev, "Error: No device match found\n");
+		return -ENODEV;
+	}
+	pdata = (struct ion_platform_data *)match->data;
 
+	num_heaps = pdata->nr;
 	heaps = devm_kzalloc(&pdev->dev,
 			     sizeof(struct ion_heap *) * pdata->nr,
 			     GFP_KERNEL);
+	if (!heaps)
+		return -ENOMEM;
 
 	idev = ion_device_create(NULL);
 	if (IS_ERR_OR_NULL(idev))
 		return PTR_ERR(idev);
 
+	if (vpr_size) {
+		pdata->heaps[ION_HEAP_TYPE_CARVEOUT].base = vpr_start;
+		pdata->heaps[ION_HEAP_TYPE_CARVEOUT].size = vpr_size;
+	}
+
 	/* create the heaps as specified in the board file */
 	for (i = 0; i < num_heaps; i++) {
 		struct ion_platform_heap *heap_data = &pdata->heaps[i];
+
+		if (heap_data->type == ION_HEAP_TYPE_CARVEOUT &&
+				!heap_data->base)
+			continue;
 
 		heaps[i] = ion_heap_create(heap_data);
 		if (IS_ERR_OR_NULL(heaps[i])) {
@@ -75,7 +140,11 @@ static int tegra_ion_remove(struct platform_device *pdev)
 static struct platform_driver ion_driver = {
 	.probe = tegra_ion_probe,
 	.remove = tegra_ion_remove,
-	.driver = { .name = "ion-tegra" }
+	.driver = {
+		.name = "ion-tegra",
+		.owner = THIS_MODULE,
+		.of_match_table = ion_of_ids,
+	},
 };
 
 module_platform_driver(ion_driver);
