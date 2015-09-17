43e614f4e9275f77adfac4500066dc9b9c0605d5
Dietmar Eggemann <dietmar.eggemann@arm.com>
2015-09-17 16:10:56 +0100

    CHROMIUM: cpufreq: Frequency invariant scheduler load-tracking support
    Implements cpufreq_scale_freq_capacity() to provide the scheduler with a
    frequency scaling correction factor for more accurate load-tracking.
    
    The factor is:
    
    	current_freq(cpu) << SCHED_CAPACITY_SHIFT / max_freq(cpu)
    
    In fact, freq_scale should be a struct cpufreq_policy data member. But
    this would require that the scheduler hot path (__update_load_avg()) would
    have to grab the cpufreq lock. This can be avoided by using per-cpu data
    initialized to SCHED_CAPACITY_SCALE for freq_scale.
    
    BUG=chrome-os-partner:44828
    TEST=Boot kernel on Oak.
    
    Change-Id: I8aa1c424a33794db78c35a80c57184d992e71d2b
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Signed-off-by: Ricky Liang <jcliang@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/321265
    Reviewed-by: Andrew Bresticker <abrestic@chromium.org>
    (cherry picked from commit 794bd374b950445695e907bdebfc38108051cd4e)

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -612,4 +612,14 @@ int cpufreq_generic_init(struct cpufreq_policy *policy,
 
 void acct_update_power(struct task_struct *p, cputime_t cputime);
 
+#else
+
+static inline void acct_update_power(struct task_struct *p, cputime_t cputime)
+{
+}
+
+#endif
+
+struct sched_domain;
+unsigned long cpufreq_scale_freq_capacity(struct sched_domain *sd, int cpu);
 #endif /* _LINUX_CPUFREQ_H */
