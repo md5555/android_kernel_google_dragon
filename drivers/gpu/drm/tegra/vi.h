99c3ba8a8b0899e3bee4700027fe268c87a40d65
Sudhir Vyas <svyas@nvidia.com>
2015-10-23 15:09:15 +0530

    CHROMIUM: drm/tegra: Add VI WD IRQ support for MFI
    Add VI watchdog IRQ support for MFI callback.
    
    BUG=chrome-os-partner:46208
    TEST=Camera launch okay, switch back/front sensor, video/still works
    
    Change-Id: I713efbce074f6f26eaca0337b2bd04efcf566413
    Signed-off-by: Sudhir Vyas <svyas@nvidia.com>
    Reviewed-on: https://chromium-review.googlesource.com/308332
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>
    (cherry picked from commit 24b862fc7c056f511024d2f25f20ffe78388288c)
    Signed-off-by: David Riley <davidriley@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/318029

--- drivers/gpu/drm/tegra/vi.h
+++ drivers/gpu/drm/tegra/vi.h
@@ -50,4 +50,36 @@
 #define T12_CSI_CSICIL_SW_SENSOR_B_RESET		0x980
 #define T12_CSI_CSICIL_SW_SENSOR_B_RESET_SENSOR_B_RESET	(1 << 0)
 
+/* VI IRQ registers */
+#define VI_CFG_INTERRUPT_MASK_0				0x8c
+#define VI_CFG_INTERRUPT_STATUS_0			0x98
+
+#define VI_CSI_0_ERROR_INT_MASK_0			0x188
+#define VI_CSI_1_ERROR_INT_MASK_0			0x288
+#define VI_CSI_2_ERROR_INT_MASK_0			0x388
+#define VI_CSI_3_ERROR_INT_MASK_0			0x488
+#define VI_CSI_4_ERROR_INT_MASK_0			0x588
+#define VI_CSI_5_ERROR_INT_MASK_0			0x688
+
+#define VI_CSI_0_ERROR_STATUS				0x184
+#define VI_CSI_1_ERROR_STATUS				0x284
+#define VI_CSI_2_ERROR_STATUS				0x384
+#define VI_CSI_3_ERROR_STATUS				0x484
+#define VI_CSI_4_ERROR_STATUS				0x584
+#define VI_CSI_5_ERROR_STATUS				0x684
+
+#define VI_CSI_0_WD_CTRL				0x18c
+#define VI_CSI_1_WD_CTRL				0x28c
+#define VI_CSI_2_WD_CTRL				0x38c
+#define VI_CSI_3_WD_CTRL				0x48c
+#define VI_CSI_4_WD_CTRL				0x58c
+#define VI_CSI_5_WD_CTRL				0x68c
+
+#define NUM_VI_CHANS					6
+
+typedef void (*callback)(void *);
+
+int tegra_vi_register_mfi_cb(callback cb, void *cb_arg);
+int tegra_vi_unregister_mfi_cb(void);
+
 #endif /* _VI_H_ */
