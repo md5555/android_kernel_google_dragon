0596e9e06f7db470ad8b72a183ee21314e232d26
Ville Syrjälä <ville.syrjala@linux.intel.com>
2015-11-09 16:48:19 +0100

    BACKPORT: drm/i915: Introduce a gmbus power domain
    Currently the gmbus code uses intel_aux_display_runtime_get/put in an
    effort to make sure the hardware is powered up sufficiently for gmbus.
    That function only takes the runtime PM reference which on VLV/CHV/BXT
    is not enough. We need the disp2d/pipe-a well on VLV/CHV and power well
    2 on BXT. So add a new power domnain for gmbus and kill off the now
    unused intel_aux_display_runtime_get/put. And change
    intel_hdmi_set_edid() to use the gmbus power domain too since that's all
    we need there.
    
    Also toss in a BUILD_BUG_ON() to catch problems if we run out of
    bits for power domains. We're already really close to the limit...
    
    [Patrik: Add gmbus string to debugfs output]
    
    Signed-off-by: Ville Syrjl <ville.syrjala@linux.intel.com>
    Reviewed-by: Patrik Jakobsson <patrik.jakobsson@linux.intel.com>
    Signed-off-by: Imre Deak <imre.deak@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1447084107-8521-5-git-send-email-patrik.jakobsson@linux.intel.com
    (cherry picked from commit f0ab43e6c338896cadee64ced3fc30a5343890d9
     git://anongit.freedesktop.org/drm-intel drm-intel-next-queued)
    Signed-off-by: Joe Konno <joe.konno@intel.com>
    
    Conflicts (re-creating backmerge context artifact introduced by:
               92907cbbef86 ("Merge tag 'v4.4-rc2' into drm-intel-next-...")
    	   Maintainer: "so that we can properly re-enable skl power
    	   wells in -next..."):
    	drivers/gpu/drm/i915/intel_runtime_pm.c
    
    BUG=chrome-os-partner:41884
    TEST=Use powertop to verify PC8 and PC10 residencies with screen On/Off
    TEST=Verify PSR status with a PSR enabled display
    
    Change-Id: I06c7fc157efa7221df3a8eee50db5838d87626ec
    Reviewed-on: https://chromium-review.googlesource.com/315856
    Commit-Ready: Marc Herbert <marc.herbert@intel.com>
    Tested-by: Prathyushi Nangia <prathyushi.nangia@intel.com>
    Reviewed-by: Kevin Strasser <kevin.strasser@intel.com>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/i915/intel_i2c.c
+++ drivers/gpu/drm/i915/intel_i2c.c
@@ -489,7 +489,7 @@ gmbus_xfer(struct i2c_adapter *adapter,
 	int i = 0, inc, try = 0, reg_offset;
 	int ret = 0;
 
-	intel_aux_display_runtime_get(dev_priv);
+	intel_display_power_get(dev_priv, POWER_DOMAIN_GMBUS);
 	mutex_lock(&dev_priv->gmbus_mutex);
 
 	if (bus->force_bit) {
@@ -603,7 +603,9 @@ timeout:
 
 out:
 	mutex_unlock(&dev_priv->gmbus_mutex);
-	intel_aux_display_runtime_put(dev_priv);
+
+	intel_display_power_put(dev_priv, POWER_DOMAIN_GMBUS);
+
 	return ret;
 }
 
