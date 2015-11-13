08db5029855f9dba06cfdffd3cf21bb0309c30ad
Subhransu S. Prusty <subhransu.s.prusty@intel.com>
2015-11-13 19:06:24 +0530

    CHROMIUM: ASoC: Intel: Skylake: Enable hdmi in passthrough
    Add required hw info and idisp BE dai formats to support HDMI.
    Also add the formats and rates support as required by HDMI
    
    Signed-off-by: Subhransu S. Prusty <subhransu.s.prusty@intel.com>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>
    Link: git://git.kernel.org/pub/scm/linux/kernel/git/vkoul/sound.git
    br: topic/skl_ww496
    (cherry picked from commit ceda6abeb77872de824c05297ff8751dad4a3df4)
    Signed-off-by: Vedang Patel <vedang.patel@intel.com>
    
    BUG=chrome-os-partner:48269
    TEST=with the whole series applied, audio works on SKL.
    TEST=there are no regressions observed on BSW.
    
    Change-Id: I5ab72a9d2f7ff88af1cfb9d159b54d108d0f6dd7
    Reviewed-on: https://chromium-review.googlesource.com/316219
    Commit-Ready: Radhakrishna Sripada <radhakrishna.sripada@intel.com>
    Tested-by: Radhakrishna Sripada <radhakrishna.sripada@intel.com>
    Reviewed-by: Derek Basehore <dbasehore@chromium.org>

--- sound/soc/intel/skylake/skl-pcm.c
+++ sound/soc/intel/skylake/skl-pcm.c
@@ -660,6 +660,21 @@ static struct snd_soc_dai_driver skl_platform_dai[] = {
 		.formats = SNDRV_PCM_FMTBIT_S16_LE | SNDRV_PCM_FMTBIT_S24_LE,
 	},
 },
+{
+	.name = "HDMI Pin",
+	.ops = &skl_pcm_dai_ops,
+	.playback = {
+		.stream_name = "HDMI Playback",
+		.channels_min = HDA_STEREO,
+		.channels_max = HDA_STEREO,
+		.rates = SNDRV_PCM_RATE_32000 |	SNDRV_PCM_RATE_44100 |
+			SNDRV_PCM_RATE_48000 | SNDRV_PCM_RATE_88200 |
+			SNDRV_PCM_RATE_96000 | SNDRV_PCM_RATE_176400 |
+			SNDRV_PCM_RATE_192000,
+		.formats = SNDRV_PCM_FMTBIT_S16_LE | SNDRV_PCM_FMTBIT_S24_LE |
+			SNDRV_PCM_FMTBIT_S32_LE,
+	},
+},
 
 /* BE CPU  Dais */
 {
@@ -706,7 +721,8 @@ static struct snd_soc_dai_driver skl_platform_dai[] = {
 		.channels_min = HDA_STEREO,
 		.channels_max = HDA_STEREO,
 		.rates = SNDRV_PCM_RATE_8000|SNDRV_PCM_RATE_16000|SNDRV_PCM_RATE_48000,
-		.formats = SNDRV_PCM_FMTBIT_S16_LE,
+		.formats = SNDRV_PCM_FMTBIT_S16_LE | SNDRV_PCM_FMTBIT_S32_LE |
+			SNDRV_PCM_FMTBIT_S24_LE,
 	},
 },
 {
