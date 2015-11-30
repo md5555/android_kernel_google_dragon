a2363f12851b14b54199ef49937709737b6197e4
Andrew Bresticker <abrestic@chromium.org>
2015-11-30 14:58:55 -0800

    CHROMIUM: usb: gadget: composite: Fix potential use-after-free
    The configfs gadget driver's struct usb_composite_dev is re-used across
    bind/unbind operations, so cdev->req and cdev->os_desc_req, which are
    freed on unbind, are stale pointers on the subsequent bind.  This is in
    issue if the subsequent bind fails since composite_dev_cleanup() will
    then dereference the stale cdev->req and cdev->os_desc_req pointers.
    Fix this by setting cdev->req and cdev->os_desc_req to NULL after
    freeing them.
    
    BUG=b:25886573
    TEST=Switch between gadget configurations on Smaug; no issues seen.
    
    Change-Id: I7a1f97470bce9a4474f70b38443fde1238fd78c6
    Signed-off-by: Andrew Bresticker <abrestic@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/314934
    Reviewed-by: Benson Leung <bleung@chromium.org>

--- drivers/usb/gadget/composite.c
+++ drivers/usb/gadget/composite.c
@@ -2005,6 +2005,7 @@ void composite_dev_cleanup(struct usb_composite_dev *cdev)
 
 		kfree(cdev->os_desc_req->buf);
 		usb_ep_free_request(cdev->gadget->ep0, cdev->os_desc_req);
+		cdev->os_desc_req = NULL;
 	}
 	if (cdev->req) {
 		if (cdev->setup_pending)
@@ -2012,6 +2013,7 @@ void composite_dev_cleanup(struct usb_composite_dev *cdev)
 
 		kfree(cdev->req->buf);
 		usb_ep_free_request(cdev->gadget->ep0, cdev->req);
+		cdev->req = NULL;
 	}
 	cdev->next_string_id = 0;
 	device_remove_file(&cdev->gadget->dev, &dev_attr_suspended);
