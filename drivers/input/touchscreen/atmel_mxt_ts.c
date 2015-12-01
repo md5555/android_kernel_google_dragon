0abeb6d82039ebefdd88e8e4985a924d5ce9264d
Dmitry Torokhov <dtor@chromium.org>
2015-11-30 16:26:57 -0800

    Revert "CHROMIUM: atmel_mxt_ts: sleep after issuing reset command"
    This reverts commit 137b5e89471d ("CHROMIUM: atmel_mxt_ts: sleep after
    issuing reset command") because it does not allow updating firmware on
    Celes: we always time out when entering bootloader mode because we miss
    CHG transition.
    
    I think we need to come up with better strategy of validating entering
    bootloader mode that also works on Samus, probably by trying to read
    bootloader status in interrupt before signalling bl_completion, but
    that's for a followup change.
    
    BUG=chrome-os-partner:44795
    TEST=run touchpad firmware update on celes and verify that it completes
         without errors:
         chromeos-touch-firmware-update.sh -d ATML0000:00 -n maxtouch-tp.fw -f
    
    Change-Id: I36d93a838ffecc8459ca8ffa0316156b1f067738
    Signed-off-by: Dmitry Torokhov <dtor@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/296914
    Reviewed-by: Benson Leung <bleung@chromium.org>

--- drivers/input/touchscreen/atmel_mxt_ts.c
+++ drivers/input/touchscreen/atmel_mxt_ts.c
@@ -505,8 +505,6 @@ static struct dentry *mxt_debugfs_root;
 
 static int mxt_calc_resolution_T9(struct mxt_data *data);
 static int mxt_calc_resolution_T100(struct mxt_data *data);
-static int mxt_check_device_present(struct mxt_data *data,
-				    bool probe_alternate);
 static void mxt_free_object_table(struct mxt_data *data);
 static int mxt_initialize(struct mxt_data *data);
 static int mxt_input_dev_create(struct mxt_data *data);
@@ -1445,26 +1443,24 @@ static int mxt_enter_bl(struct mxt_data *data)
 		data->input_dev = NULL;
 	}
 
+	enable_irq(data->irq);
+	/* Clean up message queue in device */
+	mxt_handle_messages(data, false);
+
+	disable_irq(data->irq);
+
+
 	/* Change to the bootloader mode */
 	ret = mxt_write_object(data, MXT_GEN_COMMAND_T6,
 			       MXT_COMMAND_RESET, MXT_BOOT_VALUE);
 	if (ret) {
 		dev_err(dev, "Failed to change to bootloader mode %d.\n", ret);
 		enable_irq(data->irq);
-		goto err_enter_bl;
+		return ret;
 	}
-
-	/* Wait for bootloader mode ready */
-	msleep(MXT_RESET_TIME);
 
 	/* Change to slave address of bootloader */
 	client->addr = mxt_lookup_bootloader_address(data);
-
-	ret = mxt_check_device_present(data, false);
-	if (ret) {
-		dev_err(dev, "Failed to check device in BL mode %d.\n", ret);
-		goto err_enter_bl;
-	}
 
 	init_completion(&data->bl_completion);
 	enable_irq(data->irq);
@@ -1474,13 +1470,10 @@ static int mxt_enter_bl(struct mxt_data *data)
 	if (ret) {
 		dev_err(dev, "Failed waiting for reset to bootloader %d.\n",
 			ret);
-		goto err_enter_bl;
+		client->addr = mxt_lookup_appmode_address(data);
+		return ret;
 	}
 	return 0;
-
-err_enter_bl:
-	client->addr = mxt_lookup_appmode_address(data);
-	return ret;
 }
 
 static void mxt_exit_bl(struct mxt_data *data)
