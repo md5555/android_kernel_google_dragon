34eb9e94bf6919bc5ca11ce9da7e0c3f523d54a1
Jon Hunter <jonathanh@nvidia.com>
2015-12-04 17:04:24 +0000

    FROMLIST: clk: tegra210: Fix sparse warnings for functions not declared as static
    Sparse reports the following warnings for functions in clk-tegra210.c
    that should be declared as static:
    
    drivers/clk/tegra/clk-tegra210.c:460:6: warning: symbol
     'tegra210_pllcx_set_defaults' was not declared. Should it be static?
    drivers/clk/tegra/clk-tegra210.c:485:6: warning: symbol
     '_pllc_set_defaults' was not declared. Should it be static?
    drivers/clk/tegra/clk-tegra210.c:490:6: warning: symbol
     '_pllc2_set_defaults' was not declared. Should it be static?
    drivers/clk/tegra/clk-tegra210.c:495:6: warning: symbol
     '_pllc3_set_defaults' was not declared. Should it be static?
    drivers/clk/tegra/clk-tegra210.c:500:6: warning: symbol
     '_plla1_set_defaults' was not declared. Should it be static?
    drivers/clk/tegra/clk-tegra210.c:510:6: warning: symbol
     'tegra210_plla_set_defaults' was not declared. Should it be static?
    drivers/clk/tegra/clk-tegra210.c:562:6: warning: symbol
     'tegra210_plld_set_defaults' was not declared. Should it be static?
    drivers/clk/tegra/clk-tegra210.c:701:6: warning: symbol
     'tegra210_plld2_set_defaults' was not declared. Should it be static?
    drivers/clk/tegra/clk-tegra210.c:709:6: warning: symbol
     'tegra210_plldp_set_defaults' was not declared. Should it be static?
    drivers/clk/tegra/clk-tegra210.c:722:6: warning: symbol
     'tegra210_pllc4_set_defaults' was not declared. Should it be static?
    drivers/clk/tegra/clk-tegra210.c:731:6: warning: symbol
     'tegra210_pllre_set_defaults' was not declared. Should it be static?
    drivers/clk/tegra/clk-tegra210.c:844:6: warning: symbol
     'tegra210_pllx_set_defaults' was not declared. Should it be static?
    drivers/clk/tegra/clk-tegra210.c:904:6: warning: symbol
     'tegra210_pllmb_set_defaults' was not declared. Should it be static?
    drivers/clk/tegra/clk-tegra210.c:963:6: warning: symbol
     'tegra210_pllp_set_defaults' was not declared. Should it be static?
    drivers/clk/tegra/clk-tegra210.c:1025:6: warning: symbol
     'tegra210_pllu_set_defaults' was not declared. Should it be static?
    drivers/clk/tegra/clk-tegra210.c:1215:15: warning: symbol
     'tegra210_clk_adjust_vco_min' was not declared. Should it be static?
    
    Fix this by declaring the above as static.
    
    Fixes: b31eba5ff3f7 ("clk: tegra: Add support for Tegra210 clocks")
    
    Signed-off-by: Jon Hunter <jonathanh@nvidia.com>
    
    (git am from https://patchwork.ozlabs.org/patch/552811/)
    
    BUG=chrome-os-partner:39779
    TEST=build and boot
    
    Change-Id: Ib759000e58b5d3b6da264fd7cb15a0d51ef59781
    Signed-off-by: Rhyland Klein <rklein@nvidia.com>
    Reviewed-on: https://chromium-review.googlesource.com/316890
    Reviewed-by: Andrew Bresticker <abrestic@chromium.org>
    Commit-Queue: Andrew Bresticker <abrestic@chromium.org>
    (cherry picked from commit 17f6703986f829aeb146b380cd1bd4b920c60bd9)
    Signed-off-by: David Riley <davidriley@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/318036

--- drivers/clk/tegra/clk-tegra210.c
+++ drivers/clk/tegra/clk-tegra210.c
@@ -514,7 +514,7 @@ static void pllcx_check_defaults(struct tegra_clk_pll_params *params)
 			PLLCX_MISC3_WRITE_MASK);
 }
 
-void tegra210_pllcx_set_defaults(struct tegra_clk_pll *pllcx)
+static void tegra210_pllcx_set_defaults(struct tegra_clk_pll *pllcx)
 {
 	pllcx->params->defaults_set = true;
 
@@ -544,7 +544,7 @@ void tegra210_pllcx_set_defaults(struct tegra_clk_pll *pllcx)
  * PLL with dynamic ramp and fractional SDM. Dynamic ramp is not used.
  * Fractional SDM is allowed to provide exact audio rates.
  */
-void tegra210_plla_set_defaults(struct tegra_clk_pll *plla)
+static void tegra210_plla_set_defaults(struct tegra_clk_pll *plla)
 {
 	u32 mask;
 	u32 val = readl_relaxed(clk_base + plla->params->base_reg);
@@ -597,7 +597,7 @@ void tegra210_plla_set_defaults(struct tegra_clk_pll *plla)
  * PLLD
  * PLL with fractional SDM.
  */
-void tegra210_plld_set_defaults(struct tegra_clk_pll *plld)
+static void tegra210_plld_set_defaults(struct tegra_clk_pll *plld)
 {
 	u32 val;
 	u32 mask = 0xffff;
@@ -737,7 +737,7 @@ static void plldss_defaults(struct tegra_clk_pll *plldss,
 	udelay(1);
 }
 
-void tegra210_plld2_set_defaults(struct tegra_clk_pll *plld2)
+static void tegra210_plld2_set_defaults(struct tegra_clk_pll *plld2)
 {
 	plldss_defaults(plld2, PLLD2_MISC0_DEFAULT_VALUE,
 			PLLD2_MISC1_CFG_DEFAULT_VALUE,
@@ -745,7 +745,7 @@ void tegra210_plld2_set_defaults(struct tegra_clk_pll *plld2)
 			PLLD2_MISC3_CTRL2_DEFAULT_VALUE);
 }
 
-void tegra210_plldp_set_defaults(struct tegra_clk_pll *plldp)
+static void tegra210_plldp_set_defaults(struct tegra_clk_pll *plldp)
 {
 	plldss_defaults(plldp, PLLDP_MISC0_DEFAULT_VALUE,
 			PLLDP_MISC1_CFG_DEFAULT_VALUE,
@@ -758,7 +758,7 @@ void tegra210_plldp_set_defaults(struct tegra_clk_pll *plldp)
  * Base and misc0 layout is the same as PLLD2/PLLDP, but no SDM/SSC support.
  * VCO is exposed to the clock tree via fixed 1/3 and 1/5 dividers.
  */
-void tegra210_pllc4_set_defaults(struct tegra_clk_pll *pllc4)
+static void tegra210_pllc4_set_defaults(struct tegra_clk_pll *pllc4)
 {
 	plldss_defaults(pllc4, PLLC4_MISC0_DEFAULT_VALUE, 0, 0, 0);
 }
@@ -767,7 +767,7 @@ void tegra210_pllc4_set_defaults(struct tegra_clk_pll *pllc4)
  * PLLRE
  * VCO is exposed to the clock tree directly along with post-divider output
  */
-void tegra210_pllre_set_defaults(struct tegra_clk_pll *pllre)
+static void tegra210_pllre_set_defaults(struct tegra_clk_pll *pllre)
 {
 	u32 mask;
 	u32 val = readl_relaxed(clk_base + pllre->params->base_reg);
@@ -881,7 +881,7 @@ static void pllx_check_defaults(struct tegra_clk_pll *pll)
 			PLLX_MISC5_WRITE_MASK);
 }
 
-void tegra210_pllx_set_defaults(struct tegra_clk_pll *pllx)
+static void tegra210_pllx_set_defaults(struct tegra_clk_pll *pllx)
 {
 	u32 val;
 	u32 step_a, step_b;
@@ -942,7 +942,7 @@ void tegra210_pllx_set_defaults(struct tegra_clk_pll *pllx)
 }
 
 /* PLLMB */
-void tegra210_pllmb_set_defaults(struct tegra_clk_pll *pllmb)
+static void tegra210_pllmb_set_defaults(struct tegra_clk_pll *pllmb)
 {
 	u32 mask, val = readl_relaxed(clk_base + pllmb->params->base_reg);
 
@@ -1002,7 +1002,7 @@ static void pllp_check_defaults(struct tegra_clk_pll *pll, bool enabled)
 			~mask & PLLP_MISC1_WRITE_MASK);
 }
 
-void tegra210_pllp_set_defaults(struct tegra_clk_pll *pllp)
+static void tegra210_pllp_set_defaults(struct tegra_clk_pll *pllp)
 {
 	u32 mask;
 	u32 val = readl_relaxed(clk_base + pllp->params->base_reg);
@@ -1065,7 +1065,7 @@ static void pllu_check_defaults(struct tegra_clk_pll *pll, bool hw_control)
 			~mask & PLLU_MISC1_WRITE_MASK);
 }
 
-void tegra210_pllu_set_defaults(struct tegra_clk_pll *pllu)
+static void tegra210_pllu_set_defaults(struct tegra_clk_pll *pllu)
 {
 	u32 val = readl_relaxed(clk_base + pllu->params->base_reg);
 
@@ -1272,8 +1272,9 @@ static void tegra210_clk_pll_set_gain(struct tegra_clk_pll_freq_table *cfg)
 	cfg->m *= PLL_SDM_COEFF;
 }
 
-unsigned long tegra210_clk_adjust_vco_min(struct tegra_clk_pll_params *params,
-					  unsigned long parent_rate)
+static unsigned long
+tegra210_clk_adjust_vco_min(struct tegra_clk_pll_params *params,
+			    unsigned long parent_rate)
 {
 	unsigned long vco_min = params->vco_min;
 
