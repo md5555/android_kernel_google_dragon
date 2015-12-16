c322d5e84c922f07ccb4ad84d1be02987233b91e
Lorenzo Colitti <lorenzo@google.com>
2015-12-16 12:30:03 +0900

    BACKPORT: net: diag: Add the ability to destroy a socket.
    This patch adds a SOCK_DESTROY operation, a destroy function
    pointer to sock_diag_handler, and a diag_destroy function
    pointer.  It does not include any implementation code.
    
    [Backport of net-next 64be0aed59ad519d6f2160868734f7e278290ac1]
    
    Change-Id: I24f4a157e0a2cdb267317920aa230cc6528ca072
    Signed-off-by: Lorenzo Colitti <lorenzo@google.com>
    Acked-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Reviewed-on: https://chromium-review.googlesource.com/332926
    Reviewed-by: Dmitry Torokhov <dtor@chromium.org>
    (cherry picked from commit 2cd3a1501034746f5003eedb4edcb39fbcf81b73)
    Reviewed-on: https://chromium-review.googlesource.com/334004
    Reviewed-by: Andrew Bresticker <abrestic@chromium.org>
    Commit-Queue: Andrew Bresticker <abrestic@chromium.org>
    Tested-by: Andrew Bresticker <abrestic@chromium.org>

--- include/linux/sock_diag.h
+++ include/linux/sock_diag.h
@@ -4,6 +4,8 @@
 #include <linux/user_namespace.h>
 #include <uapi/linux/sock_diag.h>
 
+#define SOCK_DESTROY_BACKPORT 21
+
 struct sk_buff;
 struct nlmsghdr;
 struct sock;
@@ -11,6 +13,7 @@ struct sock;
 struct sock_diag_handler {
 	__u8 family;
 	int (*dump)(struct sk_buff *skb, struct nlmsghdr *nlh);
+	int (*destroy)(struct sk_buff *skb, struct nlmsghdr *nlh);
 };
 
 int sock_diag_register(const struct sock_diag_handler *h);
@@ -26,4 +29,5 @@ int sock_diag_put_meminfo(struct sock *sk, struct sk_buff *skb, int attr);
 int sock_diag_put_filterinfo(bool may_report_filterinfo, struct sock *sk,
 			     struct sk_buff *skb, int attrtype);
 
+int sock_diag_destroy(struct sock *sk, int err);
 #endif
