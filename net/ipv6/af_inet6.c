6082b56f6f036ae340ff41988077c8b57d90ea34
Dylan Reid <dgreid@chromium.org>
2015-12-18 11:18:40 -0800

    CHROMIUM: net: check ns_capable(CAP_NET_RAW) in android additions
    Some locally carried patches add checks for capable(CAP_NET_RAW).  These
    predate user namespace support and cause the configuration of a network
    created in a user namepsace to break.  Replacing these with ns_capable
    calls restores the intended functionality.
    
    Change-Id: I3872b01104c80a6dd848aa001381b595757aca57
    Signed-off-by: Dylan Reid <dgreid@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/319223
    Reviewed-by: Olof Johansson <olofj@chromium.org>

--- net/ipv6/af_inet6.c
+++ net/ipv6/af_inet6.c
@@ -67,12 +67,12 @@
 #ifdef CONFIG_ANDROID_PARANOID_NETWORK
 #include <linux/android_aid.h>
 
-static inline int current_has_network(void)
+static inline int current_has_network(struct net *net)
 {
-	return in_egroup_p(AID_INET) || capable(CAP_NET_RAW);
+	return in_egroup_p(AID_INET) || ns_capable(net->user_ns, CAP_NET_RAW);
 }
 #else
-static inline int current_has_network(void)
+static inline int current_has_network(struct net *net)
 {
 	return 1;
 }
@@ -123,7 +123,7 @@ static int inet6_create(struct net *net, struct socket *sock, int protocol,
 	int try_loading_module = 0;
 	int err;
 
-	if (!current_has_network())
+	if (!current_has_network(net))
 		return -EACCES;
 
 	/* Look for the requested type/protocol pair. */
@@ -172,7 +172,8 @@ lookup_protocol:
 	}
 
 	err = -EPERM;
-	if (sock->type == SOCK_RAW && !kern && !capable(CAP_NET_RAW))
+	if (sock->type == SOCK_RAW && !kern && !ns_capable(net->user_ns,
+							   CAP_NET_RAW))
 		goto out_rcu_unlock;
 
 	sock->ops = answer->ops;
@@ -491,7 +492,7 @@ int inet6_killaddr_ioctl(struct net *net, void __user *arg) {
 	struct in6_ifreq ireq;
 	struct sockaddr_in6 sin6;
 
-	if (!capable(CAP_NET_ADMIN))
+	if (!ns_capable(net->user_ns, CAP_NET_ADMIN))
 		return -EACCES;
 
 	if (copy_from_user(&ireq, arg, sizeof(struct in6_ifreq)))
