4363db17dd442995c567b62e64d4e913726754c6
Jeeja KP <jeeja.kp@intel.com>
2015-12-18 15:12:06 +0530

    FROMLIST: ASoC: Intel: Skylake: enable interrupt as wake source in active suspend
    In active suspend, any HDA interrupt should wake the system. When device
    enters active suspend, we need to enable HDA controller interrupt as wake
    source. Similarly disable HDA controller interrupt as wake source when
    exiting active suspend.
    
    Signed-off-by: Jeeja KP <jeeja.kp@intel.com>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>
    (am from https://patchwork.kernel.org/patch/7881721/)
    
    BUG=chrome-os-partner:48269
    TEST=with the whole series applied, audio works on SKL.
    TEST=there are no regressions observed on BSW.
    
    Change-Id: I8f828d8f593638d95256da64ef05b9c475f9d265
    Signed-off-by: Fang, Yang A <yang.a.fang@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/316465
    Commit-Ready: Radhakrishna Sripada <radhakrishna.sripada@intel.com>
    Tested-by: Radhakrishna Sripada <radhakrishna.sripada@intel.com>
    Reviewed-by: Sathyanarayana Nujella <sathyanarayana.nujella@intel.com>
    Reviewed-by: Derek Basehore <dbasehore@chromium.org>

--- sound/soc/intel/skylake/skl.c
+++ sound/soc/intel/skylake/skl.c
@@ -217,6 +217,7 @@ static int skl_suspend(struct device *dev)
 	struct pci_dev *pci = to_pci_dev(dev);
 	struct hdac_ext_bus *ebus = pci_get_drvdata(pci);
 	struct skl *skl  = ebus_to_skl(ebus);
+	struct hdac_bus *bus = ebus_to_hbus(ebus);
 
 	/*
 	 * Do not suspend if streams which are marked ignore suspend are
@@ -224,6 +225,7 @@ static int skl_suspend(struct device *dev)
 	 */
 	if (skl->supend_active) {
 		snd_hdac_ext_bus_link_power_down_all(ebus);
+		enable_irq_wake(bus->irq);
 		pci_save_state(pci);
 		pci_disable_device(pci);
 		return 0;
@@ -237,6 +239,7 @@ static int skl_resume(struct device *dev)
 	struct pci_dev *pci = to_pci_dev(dev);
 	struct hdac_ext_bus *ebus = pci_get_drvdata(pci);
 	struct skl *skl  = ebus_to_skl(ebus);
+	struct hdac_bus *bus = ebus_to_hbus(ebus);
 	int ret;
 
 	/*
@@ -247,6 +250,7 @@ static int skl_resume(struct device *dev)
 		pci_restore_state(pci);
 		ret = pci_enable_device(pci);
 		snd_hdac_ext_bus_link_power_up_all(ebus);
+		disable_irq_wake(bus->irq);
 	} else {
 		ret = _skl_resume(ebus);
 	}
