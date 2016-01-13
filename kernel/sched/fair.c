a3529b7fe3c995f48a37ef2d5c24864639a1db8f
Morten Rasmussen <morten.rasmussen@arm.com>
2016-01-13 16:09:26 +0000

    CHROMIUM: sched: Add per-cpu max capacity to sched_group_capacity
    struct sched_group_capacity currently represents the compute capacity
    sum of all cpus in the sched_group. Unless it is divided by the
    group_weight to get the average capacity per cpu it hides differences in
    cpu capacity for mixed capacity systems (e.g. high RT/IRQ utilization or
    ARM big.LITTLE). But even the average may not be sufficient if the group
    covers cpus of different capacities. Instead, by extending struct
    sched_group_capacity to indicate max per-cpu capacity in the group a
    suitable group for a given task utilization can easily be found such
    that cpus with reduced capacity can be avoided for tasks with high
    utilization (not implemented by this patch).
    
    BUG=chrome-os-partner:44828
    TEST=Boot kernel on Oak.
    
    Change-Id: I3ad0e6df855b1a184db05cb310e91e1e03061467
    Signed-off-by: Morten Rasmussen <morten.rasmussen@arm.com>
    Signed-off-by: Ricky Liang <jcliang@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/292361
    Reviewed-by: Andrew Bresticker <abrestic@chromium.org>
    (cherry picked from commit eec0661accf826a46d0fcdf6d2ad6ff0e06c23da)

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6378,13 +6378,14 @@ skip_unlock: __attribute__ ((unused));
 
 	cpu_rq(cpu)->cpu_capacity = capacity;
 	sdg->sgc->capacity = capacity;
+	sdg->sgc->max_capacity = capacity;
 }
 
 void update_group_capacity(struct sched_domain *sd, int cpu)
 {
 	struct sched_domain *child = sd->child;
 	struct sched_group *group, *sdg = sd->groups;
-	unsigned long capacity;
+	unsigned long capacity, max_capacity;
 	unsigned long interval;
 
 	interval = msecs_to_jiffies(sd->balance_interval);
@@ -6397,6 +6398,7 @@ void update_group_capacity(struct sched_domain *sd, int cpu)
 	}
 
 	capacity = 0;
+	max_capacity = 0;
 
 	if (child->flags & SD_OVERLAP) {
 		/*
@@ -6407,6 +6409,7 @@ void update_group_capacity(struct sched_domain *sd, int cpu)
 		for_each_cpu(cpu, sched_group_cpus(sdg)) {
 			struct sched_group_capacity *sgc;
 			struct rq *rq = cpu_rq(cpu);
+			unsigned long cap;
 
 			/*
 			 * build_sched_domains() -> init_sched_groups_capacity()
@@ -6420,12 +6423,13 @@ void update_group_capacity(struct sched_domain *sd, int cpu)
 			 * causing divide-by-zero issues on boot.
 			 */
 			if (unlikely(!rq->sd)) {
-				capacity += capacity_of(cpu);
-				continue;
+				cap = capacity_of(cpu);
+			} else {
+				sgc = rq->sd->groups->sgc;
+				cap = sgc->capacity;
 			}
-
-			sgc = rq->sd->groups->sgc;
-			capacity += sgc->capacity;
+			capacity += cap;
+			max_capacity = max(cap, max_capacity);
 		}
 	} else  {
 		/*
@@ -6435,12 +6439,16 @@ void update_group_capacity(struct sched_domain *sd, int cpu)
 
 		group = child->groups;
 		do {
-			capacity += group->sgc->capacity;
+			struct sched_group_capacity *sgc = group->sgc;
+
+			capacity += sgc->capacity;
+			max_capacity = max(sgc->max_capacity, max_capacity);
 			group = group->next;
 		} while (group != child->groups);
 	}
 
 	sdg->sgc->capacity = capacity;
+	sdg->sgc->max_capacity = max_capacity;
 }
 
 /*
