537dccbc8eeee78b03af607d173be7685dfc3a50
Sascha Hauer <s.hauer@pengutronix.de>
2016-01-22 13:39:25 +0100

    BACKPORT: soc: mediatek: SCPSYS: Fix double enabling of regulators
    With CONFIG_PM enabled do not call genpd->power_on manually as this
    will cause the regulators being turned on once in SCPSYS probe and
    then again when the genpd core turns on the domains. Instead, call
    genpd->power_on only with CONFIG_PM disabled and tell the genpd core
    that the domains are disabled when registered.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>
    Tested-by: Daniel Kurtz <djkurtz@chromium.org>
    Reviewed-by: Daniel Kurtz <djkurtz@chromium.org>
    Signed-off-by: Matthias Brugger <matthias.bgg@gmail.com>
    (cherry picked from commit cc8ed76938b5cf6a54ab3d60edabaf808dc960d1
     https://github.com/mbgg/linux-mediatek v4.5-next/soc)
    Signed-off-by: Daniel Kurtz <djkurtz@chromium.org>
    [djkurtz: keep MFG_* domain checking since chromeos-3.18 has
     non-upstreamable commit 4762f11 ("CHROMIUM: soc: mediatek: SCPSYS:
     Drop parent/child relationship for mfg domains").
    ]
    
    BUG=chrome-os-partner:49416
    TEST=On oak-rev4:
        stop ui
        localhost ~ # cat /sys/kernel/debug/pm_genpd/pm_genpd_summary
        domain                          status          slaves
            /device                                             runtime status
            ----------------------------------------------------------------------
            mfg                             off
        localhost ~ # cat /sys/class/regulator/regulator.10/name
        vgpu
        localhost ~ # cat /sys/class/regulator/regulator.10/status
        off
    
    Change-Id: If91b45a53f6942931a6c97e9d8130a3f9ac6fd84
    Reviewed-on: https://chromium-review.googlesource.com/322044
    Commit-Ready: Daniel Kurtz <djkurtz@chromium.org>
    Tested-by: Daniel Kurtz <djkurtz@chromium.org>
    Reviewed-by: Wei-Ning Huang <wnhuang@chromium.org>

--- drivers/soc/mediatek/mtk-scpsys.c
+++ drivers/soc/mediatek/mtk-scpsys.c
@@ -537,18 +537,17 @@ static int scpsys_probe(struct platform_device *pdev)
 		genpd->dev_ops.active_wakeup = scpsys_active_wakeup;
 
 		/*
-		 * Initially turn on all domains to make the domains usable
-		 * with !CONFIG_PM and to get the hardware in sync with the
-		 * software.  The unused domains will be switched off during
-		 * late_init time.
+		 * With CONFIG_PM disabled turn on all domains to make the
+		 * hardware usable.
 		 */
-		genpd->power_on(genpd);
+		if (!IS_ENABLED(CONFIG_PM))
+			genpd->power_on(genpd);
 
 		if (i == MT8173_POWER_DOMAIN_MFG_2D ||
 		    i == MT8173_POWER_DOMAIN_MFG_ASYNC)
 			continue;
 
-		pm_genpd_init(genpd, NULL, false);
+		pm_genpd_init(genpd, NULL, true);
 	}
 
 	/*
