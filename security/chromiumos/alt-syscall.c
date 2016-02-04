1925ea0c4fb062bb2b8545a268b27ccb4fb1ae40
Andrew Bresticker <abrestic@chromium.org>
2016-02-04 12:51:54 -0800

    CHROMIUM: alt-syscall: Whitelist sched_get_priority_{min,max}()
    sched_get_priority_{min,max}(2) are apparently used by the game CSR
    Racing.  Whitelist these syscalls since they are harmless and will only
    ever return a static range on Linux anyway.
    
    BUG=b:26589008
    TEST=Builds for Cyan and Oak.
    
    Change-Id: I9cc9317fe85c9389f02bd7fb67905be97976c36b
    Signed-off-by: Andrew Bresticker <abrestic@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/325865
    Reviewed-by: Kevin Cernekee <cernekee@chromium.org>

--- security/chromiumos/alt-syscall.c
+++ security/chromiumos/alt-syscall.c
@@ -270,6 +270,8 @@ static asmlinkage long alt_sys_prctl(int option, unsigned long arg2,
 #define __NR_compat_rt_sigsuspend	__NR_ia32_rt_sigsuspend
 #define __NR_compat_rt_sigtimedwait	__NR_ia32_rt_sigtimedwait
 #define __NR_compat_rt_tgsigqueueinfo	__NR_ia32_rt_tgsigqueueinfo
+#define __NR_compat_sched_get_priority_max	__NR_ia32_sched_get_priority_max
+#define __NR_compat_sched_get_priority_min	__NR_ia32_sched_get_priority_min
 #define __NR_compat_sched_getparam	__NR_ia32_sched_getparam
 #define __NR_compat_sched_getscheduler	__NR_ia32_sched_getscheduler
 #define __NR_compat_sched_setscheduler	__NR_ia32_sched_setscheduler
@@ -494,6 +496,8 @@ static struct syscall_whitelist_entry android_whitelist[] = {
 	SYSCALL_ENTRY(rt_sigsuspend),
 	SYSCALL_ENTRY(rt_sigtimedwait),
 	SYSCALL_ENTRY(rt_tgsigqueueinfo),
+	SYSCALL_ENTRY(sched_get_priority_max),
+	SYSCALL_ENTRY(sched_get_priority_min),
 	SYSCALL_ENTRY(sched_getparam),
 	SYSCALL_ENTRY(sched_getscheduler),
 	SYSCALL_ENTRY(sched_setscheduler),
@@ -810,6 +814,8 @@ static struct syscall_whitelist_entry android_compat_whitelist[] = {
 	COMPAT_SYSCALL_ENTRY(rt_sigsuspend),
 	COMPAT_SYSCALL_ENTRY(rt_sigtimedwait),
 	COMPAT_SYSCALL_ENTRY(rt_tgsigqueueinfo),
+	COMPAT_SYSCALL_ENTRY(sched_get_priority_max),
+	COMPAT_SYSCALL_ENTRY(sched_get_priority_min),
 	COMPAT_SYSCALL_ENTRY(sched_getparam),
 	COMPAT_SYSCALL_ENTRY(sched_getscheduler),
 	COMPAT_SYSCALL_ENTRY(sched_setscheduler),
