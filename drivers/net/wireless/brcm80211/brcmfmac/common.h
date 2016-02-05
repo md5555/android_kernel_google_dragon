67531eb00b1663ea9170a1967bf48ff1a786a63f
simplicit <simplicit@olivierk.eu>
2016-02-05 14:27:36 +0100

    Use v4.1 driver

--- drivers/net/wireless/brcm80211/brcmfmac/common.h
+++ drivers/net/wireless/brcm80211/brcmfmac/common.h
@@ -1,5 +1,4 @@
-/*
- * Copyright (c) 2011 Broadcom Corporation
+/* Copyright (c) 2014 Broadcom Corporation
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -13,28 +12,9 @@
  * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
  * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
+#ifndef BRCMFMAC_COMMON_H
+#define BRCMFMAC_COMMON_H
 
-#ifndef __BRCMS_TRACE_EVENTS_H
-#define __BRCMS_TRACE_EVENTS_H
+extern const u8 ALLFFMAC[ETH_ALEN];
 
-#include <linux/types.h>
-#include <linux/device.h>
-#include <linux/tracepoint.h>
-#include "mac80211_if.h"
-
-#ifndef CONFIG_BRCM_TRACING
-#undef TRACE_EVENT
-#define TRACE_EVENT(name, proto, ...) \
-static inline void trace_ ## name(proto) {}
-#undef DECLARE_EVENT_CLASS
-#define DECLARE_EVENT_CLASS(...)
-#undef DEFINE_EVENT
-#define DEFINE_EVENT(evt_class, name, proto, ...) \
-static inline void trace_ ## name(proto) {}
-#endif
-
-#include "brcms_trace_brcmsmac.h"
-#include "brcms_trace_brcmsmac_tx.h"
-#include "brcms_trace_brcmsmac_msg.h"
-
-#endif /* __TRACE_BRCMSMAC_H */
+#endif /* BRCMFMAC_COMMON_H */
