9c71c4a6b4f5627d13d0be2c414e74a28a121fd2
simplicit <simplicit@olivierk.eu>
2016-02-28 15:50:02 +0100

    cpufreq: Port cpu-boost from franco/angler

--- drivers/gpu/drm/panel/panel-jdi-lpm102a188a.c
+++ drivers/gpu/drm/panel/panel-jdi-lpm102a188a.c
@@ -87,6 +87,12 @@ struct panel_jdi {
 	u8 current_register;
 };
 
+static bool display_on = false;
+
+bool is_display_on(void) {
+	return display_on;
+}
+
 static inline struct panel_jdi *to_panel_jdi(struct drm_panel *panel)
 {
 	return container_of(panel, struct panel_jdi, base);
@@ -311,6 +317,8 @@ static int panel_jdi_disable(struct drm_panel *panel)
 	msleep(150);
 
 	jdi->brightness = 0;
+
+	display_on = false;
 out:
 	mutex_unlock(&jdi->lock);
 	return ret;
@@ -550,6 +558,8 @@ static int panel_jdi_enable(struct drm_panel *panel)
 	}
 
 	jdi->enabled = true;
+	
+	display_on = true;
 
 	mutex_unlock(&jdi->lock);
 
