b61f12b0ea91577d5322c585ccd0ec48574c4af1
simplicit <simplicit@olivierk.eu>
2016-03-12 13:03:42 +0100

    Fix imx208.c compilation

--- drivers/gpu/drm/tegra/camera/imx208.c
+++ drivers/gpu/drm/tegra/camera/imx208.c
@@ -660,32 +660,6 @@ check_fail:
 	return err;
 }
 
-static int imx208_check_module(struct imx208_info *info)
-{
-	u8 module_id[2];
-	int err;
-	struct imx208_power_rail *pw = &info->power;
-
-	err = imx208_power_on(pw);
-	if (err)
-		return err;
-
-	err = imx208_read_reg(info, 0x0000, &module_id[0]);
-	if (err)
-		goto check_fail;
-
-	err = imx208_read_reg(info, 0x0001, &module_id[1]);
-	if (err)
-		goto check_fail;
-
-	if (module_id[0] != 0x02 || module_id[1] != 0x08)
-		err = -ENODEV;
-
-check_fail:
-	imx208_power_off(pw);
-	return err;
-}
-
 static int
 imx208_probe(struct i2c_client *client, const struct i2c_device_id *id)
 {
