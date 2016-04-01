2e156ab0ba269a557b333bb5294ba923d2af5a95
David Riley <davidriley@google.com>
2016-04-01 13:31:13 -0700

    CHROMIUM: dragon: make-fit-image: compress with lz4
    Change-Id: Ie51d2d0e3f0ef18469a7cade4e1fb8b167f3d795

--- chromeos/scripts/make-fit-image.sh
+++ chromeos/scripts/make-fit-image.sh
@@ -4,11 +4,18 @@ OUT_DIR=$1
 KERNEL_DIR=$2
 ARCH=$3
 DTB_DIR=$4
+LZ4_PATH=$5
+
+compression="none"
+if [ -d "${LZ4_PATH}" ]; then
+	export PATH=${LZ4_PATH}:${PATH}
+	compression="lz4"
+fi
 
 cd ${OUT_DIR} 
 
 ${KERNEL_DIR}/chromeos/scripts/generate-its-script.sh \
-	-a ${ARCH} arch/${ARCH}/boot/Image ${DTB_DIR}/*.dtb \
+	-a ${ARCH} -c ${compression} arch/${ARCH}/boot/Image ${DTB_DIR}/*.dtb \
 	| ${OUT_DIR}/scripts/dtc/dtc -I dts -O dtb -p 1024 \
 	> ${OUT_DIR}/arch/${ARCH}/boot/Image.fit
 
