d73464a976b39abbf70049be19f123005dbbb4ef
David Riley <davidriley@google.com>
2016-04-01 12:56:55 -0700

    CHROMIUM: dragon: add make-fit-image.sh to create Image.fit
    (cherry picked from commit e5df30705aea165eb95790ea1287b13904a17815)
    
    Change-Id: Ibc59ee1b1fa625bdfca32279785c7a525e859472

---
+++ chromeos/scripts/make-fit-image.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+OUT_DIR=$1
+KERNEL_DIR=$2
+ARCH=$3
+DTB_DIR=$4
+
+cd ${OUT_DIR} 
+
+${KERNEL_DIR}/chromeos/scripts/generate-its-script.sh \
+	-a ${ARCH} arch/${ARCH}/boot/Image ${DTB_DIR}/*.dtb \
+	| ${OUT_DIR}/scripts/dtc/dtc -I dts -O dtb -p 1024 \
+	> ${OUT_DIR}/arch/${ARCH}/boot/Image.fit
+
