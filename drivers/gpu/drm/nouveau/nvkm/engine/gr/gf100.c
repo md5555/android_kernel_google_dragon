0b762f4f206569d1b6ce5801215fefd2f85d2973
Vince Hsu <vince.h@nvidia.com>
2016-05-03 10:07:44 +0800

    CHROMIUM: drm/nouveau/gr/gf100: speed up GR context creation
    When allocating context we don't necessarily initialize the memory as
    zeros because the initialization will be done later in the same function.
    Removing the flag NVOBJ_FLAG_ZERO_ALLOC can reduce the GR context
    creation time significantly.
    
    Change-Id: Iae6f077d180541a5b56617718579838e0a33d7c7
    Signed-off-by: Vince Hsu <vince.h@nvidia.com>
    Reviewed-on: https://chromium-review.googlesource.com/341831
    Tested-by: Stéphane Marchesin <marcheu@chromium.org>
    Tested-by: Tony Zlatinski <tzlatinski@nvidia.com>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    Reviewed-by: Tony Zlatinski <tzlatinski@nvidia.com>
    (cherry picked from commit 39208d100cf503fb39430324a012bb1db8ecc774)
    Reviewed-on: https://chromium-review.googlesource.com/351875
    Reviewed-by: Stephen Barber <smbarber@chromium.org>
    Commit-Queue: Stephen Barber <smbarber@chromium.org>
    Tested-by: Stephen Barber <smbarber@chromium.org>

--- drivers/gpu/drm/nouveau/nvkm/engine/gr/gf100.c
+++ drivers/gpu/drm/nouveau/nvkm/engine/gr/gf100.c
@@ -466,7 +466,7 @@ gf100_gr_context_ctor(struct nvkm_object *parent, struct nvkm_object *engine,
 	/* allocate memory for context, and fill with default values */
 	ret = nvkm_gr_context_create(parent, engine, oclass, NULL,
 				     priv->size, 0x100,
-				     NVOBJ_FLAG_ZERO_ALLOC, &chan);
+				     0x0, &chan);
 	*pobject = nv_object(chan);
 	if (ret)
 		return ret;
