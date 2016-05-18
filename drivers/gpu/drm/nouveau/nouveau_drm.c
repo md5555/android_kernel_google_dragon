07d3fe1f98ff632fa8e12b19509108ca60085973
Vince Hsu <vince.h@nvidia.com>
2016-05-18 14:57:11 +0800

    CHROMIUM: drm/nouveau: add more messages for nouveau PM
    So that we know the suspend/resume process is done or not.
    
    Change-Id: Ib4610573675436d6270d573cd9290821683043a3
    Signed-off-by: Vince Hsu <vince.h@nvidia.com>
    Reviewed-on: https://chromium-review.googlesource.com/345772
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    (cherry picked from commit f32f22bf5f3c10ac6c4a60fb866225d1937a4f91)
    Reviewed-on: https://chromium-review.googlesource.com/354708
    Reviewed-by: Stephen Barber <smbarber@chromium.org>
    Commit-Queue: Stephen Barber <smbarber@chromium.org>
    Tested-by: Stephen Barber <smbarber@chromium.org>

--- drivers/gpu/drm/nouveau/nouveau_drm.c
+++ drivers/gpu/drm/nouveau/nouveau_drm.c
@@ -628,6 +628,8 @@ nouveau_do_suspend(struct drm_device *dev, bool runtime)
 		goto fail_client;
 
 	nouveau_agp_fini(drm);
+
+	NV_INFO(drm, "nouveau suspended\n");
 	return 0;
 
 fail_client:
@@ -679,6 +681,7 @@ nouveau_do_resume(struct drm_device *dev, bool runtime)
 		nouveau_fbcon_set_suspend(dev, 0);
 	}
 
+	NV_INFO(drm, "nouveau resumed\n");
 	return 0;
 }
 
