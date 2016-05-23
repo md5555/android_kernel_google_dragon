c0a80ced22c71600f060944cd16e5c8c150ea72a
Stephen Barber <smbarber@chromium.org>
2016-05-23 13:49:27 -0700

    CHROMIUM: spidev: fix heap overflow vulnerability
    BUG=b:28430009
    TEST=none
    
    Signed-off-by: Stephen Barber <smbarber@chromium.org>
    Reviewed-on: https://chrome-internal-review.googlesource.com/260165
    Reviewed-by: Dmitry Torokhov <dtor@google.com>
    Commit-Queue: Stephen Barber <smbarber@google.com>
    Tested-by: Stephen Barber <smbarber@google.com>
    
    Change-Id: I286feff59d8a820e90acd7fa5424b35b04c065c7
    Reviewed-on: https://chromium-review.googlesource.com/350140
    Commit-Ready: Stephen Barber <smbarber@chromium.org>
    Tested-by: Stephen Barber <smbarber@chromium.org>
    Reviewed-by: Dmitry Torokhov <dtor@chromium.org>
    (cherry picked from commit 22f7042b85df8af6ef2e3df1867de48c99cf37b6)
    Reviewed-on: https://chromium-review.googlesource.com/354081
    Reviewed-by: Stephen Barber <smbarber@chromium.org>
    Commit-Queue: Stephen Barber <smbarber@chromium.org>

--- drivers/spi/spidev.c
+++ drivers/spi/spidev.c
@@ -248,11 +248,21 @@ static int spidev_message(struct spidev_data *spidev,
 	 *  Check to see if we should use preferred alignment
 	 *  Don't use if we exceed maxiumum buffer size
 	 */
-	for (n = 0; n < n_xfers; n++)
-		total += round_up(u_xfers->len, alignment);
+	for (n = 0; n < n_xfers; n++) {
+		u32 aligned_len = round_up(u_xfers[n].len, alignment);
 
-	if (total >= bufsiz)
-		alignment = 1;
+		if (aligned_len < u_xfers[n].len) {
+			alignment = 1;
+			break;
+		}
+
+		total += aligned_len;
+
+		if (total < aligned_len || total > bufsiz) {
+			alignment = 1;
+			break;
+		}
+	}
 
 	total = 0;
 	for (n = n_xfers, k_tmp = k_xfers, u_tmp = u_xfers;
@@ -262,7 +272,7 @@ static int spidev_message(struct spidev_data *spidev,
 
 		total += round_up(k_tmp->len, alignment);
 
-		if (total > bufsiz) {
+		if (total < k_tmp->len || total > bufsiz) {
 			status = -EMSGSIZE;
 			goto done;
 		}
