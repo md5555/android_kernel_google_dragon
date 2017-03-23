0e11f1ed38cdb3ec88df7631ddf83d84ca8e3ce4
Francisco Franco <franciscofranco.1990@gmail.com>
2017-03-23 03:18:13 +0000

    drivers: wakeup: more thoroughly deactivation of wakelocks
    For some reason deactivating the selected wakelocks wasn't being effective, some slipped through
    specially the ones that were acquired from an external module (no idea why or how).
    With this change they'll always be deactivated if they are active in the resume<->suspend
    routine, no chance to survive.
    
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -667,7 +667,6 @@ void dpm_resume_early(pm_message_t state)
 	struct device *dev;
 	ktime_t starttime = ktime_get();
 
-	trace_suspend_resume(TPS("dpm_resume_early"), state.event, true);
 	mutex_lock(&dpm_list_mtx);
 	pm_transition = state;
 
